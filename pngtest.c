629330e761dc23c95e65156abb8c39fc6c594382
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2006-04-17 14:33:23 -0500

    Imported from libpng-1.2.10beta6.tar

--- pngtest.c
+++ pngtest.c
@@ -1549,4 +1549,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_10beta5 your_png_h_is_not_version_1_2_10beta5;
+typedef version_1_2_10beta6 your_png_h_is_not_version_1_2_10beta6;
