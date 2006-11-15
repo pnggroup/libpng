efe95a6953205daffa287ad34ff4cf5380f4b152
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2006-11-15 06:51:50 -0600

    Imported from libpng-1.2.13.tar

--- png.h
+++ png.h
@@ -1,7 +1,7 @@
 
 /* png.h - header file for PNG reference library
  *
- * libpng version 1.2.13rc2 - November 13, 2006
+ * libpng version 1.2.13 - November 15, 2006
  * Copyright (c) 1998-2006 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -9,7 +9,7 @@
  * Authors and maintainers:
  *  libpng versions 0.71, May 1995, through 0.88, January 1996: Guy Schalnat
  *  libpng versions 0.89c, June 1996, through 0.96, May 1997: Andreas Dilger
- *  libpng versions 0.97, January 1998, through 1.2.13rc2 - November 13, 2006: Glenn
+ *  libpng versions 0.97, January 1998, through 1.2.13 - November 15, 2006: Glenn
  *  See also "Contributing Authors", below.
  *
  * Note about libpng version numbers:
@@ -126,8 +126,9 @@
  *    1.2.11                  13    10211  12.so.0.11[.0]
  *    1.0.20                  10    10020  10.so.0.20[.0]
  *    1.2.12                  13    10212  12.so.0.12[.0]
- *    1.0.21rc1-2             10    10021  10.so.0.21[.0]
- *    1.2.13rc1-2             13    10213  12.so.0.13[.0]
+ *    1.2.13beta1             13    10211  12.so.0.11[.0]
+ *    1.0.21                  10    10021  10.so.0.21[.0]
+ *    1.2.13                  13    10213  12.so.0.13[.0]
  *
  *    Henceforth the source version will match the shared-library major
  *    and minor numbers; the shared-library major version number will be
@@ -157,7 +158,7 @@
  * If you modify libpng you may insert additional notices immediately following
  * this sentence.
  *
- * libpng versions 1.2.6, August 15, 2004, through 1.2.13rc2, November 13, 2006, are
+ * libpng versions 1.2.6, August 15, 2004, through 1.2.13, November 15, 2006, are
  * Copyright (c) 2004, 2006 Glenn Randers-Pehrson, and are
  * distributed according to the same disclaimer and license as libpng-1.2.5
  * with the following individual added to the list of Contributing Authors:
@@ -269,13 +270,13 @@
  * Y2K compliance in libpng:
  * =========================
  *
- *    November 13, 2006
+ *    November 15, 2006
  *
  *    Since the PNG Development group is an ad-hoc body, we can't make
  *    an official declaration.
  *
  *    This is your unofficial assurance that libpng from version 0.71 and
- *    upward through 1.2.13rc2 are Y2K compliant.  It is my belief that earlier
+ *    upward through 1.2.13 are Y2K compliant.  It is my belief that earlier
  *    versions were also Y2K compliant.
  *
  *    Libpng only has three year fields.  One is a 2-byte unsigned integer
@@ -331,9 +332,9 @@
  */
 
 /* Version information for png.h - this should match the version in png.c */
-#define PNG_LIBPNG_VER_STRING "1.2.13rc2"
+#define PNG_LIBPNG_VER_STRING "1.2.13"
 #define PNG_HEADER_VERSION_STRING \
-   " libpng version 1.2.13rc2 - November 13, 2006 (header)\n"
+   " libpng version 1.2.13 - November 15, 2006 (header)\n"
 
 #define PNG_LIBPNG_VER_SONUM   0
 #define PNG_LIBPNG_VER_DLLNUM  13
@@ -345,7 +346,7 @@
 /* This should match the numeric part of the final component of
  * PNG_LIBPNG_VER_STRING, omitting any leading zero: */
 
-#define PNG_LIBPNG_VER_BUILD  2
+#define PNG_LIBPNG_VER_BUILD  0
 
 /* Release Status */
 #define PNG_LIBPNG_BUILD_ALPHA    1
@@ -362,7 +363,7 @@
 #define PNG_LIBPNG_BUILD_SPECIAL 32 /* Cannot be OR'ed with
                                        PNG_LIBPNG_BUILD_PRIVATE */
 
-#define PNG_LIBPNG_BUILD_BASE_TYPE PNG_LIBPNG_BUILD_RC
+#define PNG_LIBPNG_BUILD_BASE_TYPE PNG_LIBPNG_BUILD_STABLE
 
 /* Careful here.  At one time, Guy wanted to use 082, but that would be octal.
  * We must not include leading zeros.
@@ -1375,7 +1376,7 @@ struct png_struct_def
 /* This triggers a compiler error in png.c, if png.c and png.h
  * do not agree upon the version number.
  */
-typedef png_structp version_1_2_13rc2;
+typedef png_structp version_1_2_13;
 
 typedef png_struct FAR * FAR * png_structpp;
 
--- pngtest.c
+++ pngtest.c
@@ -1549,4 +1549,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_13rc2 your_png_h_is_not_version_1_2_13rc2;
+typedef version_1_2_13 your_png_h_is_not_version_1_2_13;
