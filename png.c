b8e76b586ee438dba3408c31d5424332399d15e7
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2006-11-20 07:55:23 -0600

    Imported from libpng-1.2.14rc1.tar

--- png.c
+++ png.c
@@ -13,7 +13,7 @@
 #include "png.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_14beta2 Your_png_h_is_not_version_1_2_14beta2;
+typedef version_1_2_14rc1 Your_png_h_is_not_version_1_2_14rc1;
 
 /* Version information for C files.  This had better match the version
  * string defined in png.h.  */
@@ -701,7 +701,7 @@ png_charp PNGAPI
 png_get_copyright(png_structp png_ptr)
 {
    if (&png_ptr != NULL)  /* silence compiler warning about unused png_ptr */
-   return ((png_charp) "\n libpng version 1.2.14beta2 - November 17, 2006\n\
+   return ((png_charp) "\n libpng version 1.2.14rc1 - November 20, 2006\n\
    Copyright (c) 1998-2006 Glenn Randers-Pehrson\n\
    Copyright (c) 1996-1997 Andreas Dilger\n\
    Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.\n");
