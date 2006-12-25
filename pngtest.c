295e9df783be10115dbe20ff2c578ca92e742c31
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2006-12-25 13:29:43 -0600

    [libpng10] Imported from libpng-1.0.23rc3.tar

--- pngtest.c
+++ pngtest.c
@@ -1547,4 +1547,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_0_23rc2 your_png_h_is_not_version_1_0_23rc2;
+typedef version_1_0_23rc3 your_png_h_is_not_version_1_0_23rc3;
