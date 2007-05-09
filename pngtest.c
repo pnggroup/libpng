999bceb4133e579eb8c930c88f119a31fd04b90b
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2007-05-09 14:08:41 -0500

    Imported from libpng-1.2.17rc3.tar

--- pngtest.c
+++ pngtest.c
@@ -1551,4 +1551,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_17rc2 your_png_h_is_not_version_1_2_17rc2;
+typedef version_1_2_17rc3 your_png_h_is_not_version_1_2_17rc3;
