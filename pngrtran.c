918dc976d5c773b42579c199387391089b9e1b20
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2007-05-11 23:10:45 -0500

    [libpng10] Imported from libpng-1.0.25rc2.tar

--- pngrtran.c
+++ pngrtran.c
@@ -583,7 +583,7 @@ png_set_expand_gray_1_2_4_to_8(png_structp png_ptr)
 {
    png_debug(1, "in png_set_expand_gray_1_2_4_to_8\n");
    if(png_ptr == NULL) return;
-   png_ptr->transformations |= PNG_EXPAND_tRNS;
+   png_ptr->transformations |= PNG_EXPAND;
 }
 #endif
 
