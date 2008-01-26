5c58e3ee31b986c1f337f7ba9ee9d6f1eb5211b7
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2008-01-26 10:53:35 -0600

    Imported from libpng-1.2.25beta04.tar

--- pngpread.c
+++ pngpread.c
@@ -1,7 +1,7 @@
 
 /* pngpread.c - read a png file in push mode
  *
- * Last changed in libpng 1.2.25 [January 22, 2008]
+ * Last changed in libpng 1.2.25 [January 26, 2008]
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2008 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
@@ -1026,24 +1026,26 @@ png_read_push_finish_row(png_structp png_ptr)
 
          if (png_ptr->pass > 7)
             png_ptr->pass--;
-         if (png_ptr->pass >= 7)
-            break;
+         if (png_ptr->pass < 7)
+           {
+             png_ptr->iwidth = (png_ptr->width +
+                png_pass_inc[png_ptr->pass] - 1 -
+                png_pass_start[png_ptr->pass]) /
+                png_pass_inc[png_ptr->pass];
 
-         png_ptr->iwidth = (png_ptr->width +
-            png_pass_inc[png_ptr->pass] - 1 -
-            png_pass_start[png_ptr->pass]) /
-            png_pass_inc[png_ptr->pass];
+             png_ptr->irowbytes = PNG_ROWBYTES(png_ptr->pixel_depth,
+                png_ptr->iwidth) + 1;
 
-         png_ptr->irowbytes = PNG_ROWBYTES(png_ptr->pixel_depth,
-            png_ptr->iwidth) + 1;
+             if (png_ptr->transformations & PNG_INTERLACE)
+                break;
 
-         if (png_ptr->transformations & PNG_INTERLACE)
-            break;
-
-         png_ptr->num_rows = (png_ptr->height +
-            png_pass_yinc[png_ptr->pass] - 1 -
-            png_pass_ystart[png_ptr->pass]) /
-            png_pass_yinc[png_ptr->pass];
+             png_ptr->num_rows = (png_ptr->height +
+                png_pass_yinc[png_ptr->pass] - 1 -
+                png_pass_ystart[png_ptr->pass]) /
+                png_pass_yinc[png_ptr->pass];
+           }
+         else
+           break;
 
       } while (png_ptr->iwidth == 0 || png_ptr->num_rows == 0);
    }
