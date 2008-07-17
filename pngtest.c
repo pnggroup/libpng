b8bccdd0814a52d7f5c700959e8f553f3ac21c11
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2008-07-17 13:54:41 -0500

    Imported from libpng-1.2.30rc01.tar

--- pngtest.c
+++ pngtest.c
@@ -1,7 +1,7 @@
 
 /* pngtest.c - a simple test program to test libpng
  *
- * Last changed in libpng 1.2.30 [July 10, 2008]
+ * Last changed in libpng 1.2.30 [July 17, 2008]
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2008 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
@@ -1684,4 +1684,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_30beta04 your_png_h_is_not_version_1_2_30beta04;
+typedef version_1_2_30rc01 your_png_h_is_not_version_1_2_30rc01;
