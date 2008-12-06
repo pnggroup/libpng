5ca69e43ec6143c182a57557d2be7e697e3f9e85
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2008-12-06 05:38:27 -0600

    Imported from libpng-1.4.0beta44.tar

--- pngget.c
+++ pngget.c
@@ -1,7 +1,7 @@
 
 /* pngget.c - retrieval of values from info struct
  *
- * Last changed in libpng 1.4.0 [December 2, 2008]
+ * Last changed in libpng 1.4.0 [December 6, 2008]
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2008 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
