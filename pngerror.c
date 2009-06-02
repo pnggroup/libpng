424c98547ddcf8ff81f79023add37d3dcc9a3ae4
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-06-02 13:45:15 -0500

    Imported from libpng-1.4.0beta62.tar

--- pngerror.c
+++ pngerror.c
@@ -1,7 +1,7 @@
 
 /* pngerror.c - stub functions for i/o and memory allocation
  *
- * Last changed in libpng 1.4.0 [May 31, 2009]
+ * Last changed in libpng 1.4.0 [June 2, 2009]
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
