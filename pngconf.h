b7e4c1c2e26427f4819a004f317c587a6966b15c
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-08-26 12:00:13 -0500

    [devel] Bump version to libpng-1.4.0beta77

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.4.0beta76 - August 22, 2009
+ * libpng version 1.4.0beta77 - August 26, 2009
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- pngwrite.c
+++ pngwrite.c
@@ -1,7 +1,7 @@
 
 /* pngwrite.c - general routines to write a PNG file
  *
- * Last changed in libpng 1.4.0 [August 22, 2009]
+ * Last changed in libpng 1.4.0 [August 26, 2009]
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
