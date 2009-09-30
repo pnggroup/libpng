b91c634df127813c35cbdc0daaf16833b29fee03
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-09-30 16:00:21 -0500

    [devel] Bump version to 1.4.0beta85

--- pngtest.c
+++ pngtest.c
@@ -1619,4 +1619,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_4_0beta84 your_png_h_is_not_version_1_4_0beta84;
+typedef version_1_4_0beta85 your_png_h_is_not_version_1_4_0beta85;
