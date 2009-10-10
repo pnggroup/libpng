89e0d95883ed99661d73db4a7015fe71fe90e7c2
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-10-10 06:13:58 -0500

    [master] Bump version to 1.2.41beta05

--- png.c
+++ png.c
@@ -1,7 +1,7 @@
 
 /* png.c - location for general purpose libpng functions
  *
- * Last changed in libpng 1.2.41 [October 7, 2009]
+ * Last changed in libpng 1.2.41 [October 10, 2009]
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -16,7 +16,7 @@
 #include "png.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_41beta04 Your_png_h_is_not_version_1_2_41beta04;
+typedef version_1_2_41beta05 Your_png_h_is_not_version_1_2_41beta05;
 
 /* Version information for C files.  This had better match the version
  * string defined in png.h.  */
@@ -705,7 +705,7 @@ png_charp PNGAPI
 png_get_copyright(png_structp png_ptr)
 {
    png_ptr = png_ptr;  /* Silence compiler warning about unused png_ptr */
-   return ((png_charp) "\n libpng version 1.2.41beta04 - October 7, 2009\n\
+   return ((png_charp) "\n libpng version 1.2.41beta05 - October 10, 2009\n\
    Copyright (c) 1998-2009 Glenn Randers-Pehrson\n\
    Copyright (c) 1996-1997 Andreas Dilger\n\
    Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.\n");
