819196555528ae5547c168781bb1ecd5f3f69ffa
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-10-19 06:39:35 -0500

    [master] Fixed "DATE" comment in pngrtran.c

--- pngrtran.c
+++ pngrtran.c
@@ -1,7 +1,7 @@
 
 /* pngrtran.c - transforms the data in a row for PNG readers
  *
- * Last changed in libpng 1.2.41 [%DATE]
+ * Last changed in libpng 1.2.41 [October 19, 2009]
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
