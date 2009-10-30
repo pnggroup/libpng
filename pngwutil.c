bc2ab12f99f4def34b8b510870c54bccb652b08d
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-10-29 23:40:17 -0500

    [master] Bump version to 1.2.41beta08

--- pngwutil.c
+++ pngwutil.c
@@ -1,7 +1,7 @@
 
 /* pngwutil.c - utilities to write a PNG file
  *
- * Last changed in libpng 1.2.41 [October 21, 2009]
+ * Last changed in libpng 1.2.41 [October 30, 2009]
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
