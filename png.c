38453101f0c8ad9cbb005a0d51c2209ba7f3f72a
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-11-02 06:24:56 -0600

    [master] Bump version to 1.2.41beta13

--- png.c
+++ png.c
@@ -16,7 +16,7 @@
 #include "png.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_41beta12 Your_png_h_is_not_version_1_2_41beta12;
+typedef version_1_2_41beta13 Your_png_h_is_not_version_1_2_41beta13;
 
 /* Version information for C files.  This had better match the version
  * string defined in png.h.
@@ -722,13 +722,13 @@ png_get_copyright(png_structp png_ptr)
 #else
 #ifdef __STDC__
    return ((png_charp) PNG_STRING_NEWLINE \
-     "libpng version 1.2.41beta12 - November 2, 2009" PNG_STRING_NEWLINE \
+     "libpng version 1.2.41beta13 - November 2, 2009" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2009 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE);
 #else
-      return ((png_charp) "libpng version 1.2.41beta12 - November 2, 2009\
+      return ((png_charp) "libpng version 1.2.41beta13 - November 2, 2009\
       Copyright (c) 1998-2009 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.");
