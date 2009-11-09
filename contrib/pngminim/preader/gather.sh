336b43d278d0ce1ea86998abfda45b98752bd092
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-11-09 13:39:37 -0600

    [master] Moved "rm pnggccrd.c pnggvrd.c" to a separate line
    in contrib/pngminim/*/gather.sh

--- contrib/pngminim/preader/gather.sh
+++ contrib/pngminim/preader/gather.sh
@@ -2,7 +2,8 @@ cp ../../gregbook/rpng2-x.c ../../gregbook/readpng2.[ch] .
 cp ../../gregbook/COPYING ../../gregbook/LICENSE .
 cp ../../../*.h .
 cp ../../../*.c .
-rm example.c pnggccrd.c pngvcrd.c pngtest.c pngw*.c
+rm pnggccrd.c pngvcrd.c
+rm example.c pngtest.c pngw*.c
 # change the following 2 lines if zlib is somewhere else
 cp ../../../../zlib/*.h .
 cp ../../../../zlib/*.c .
