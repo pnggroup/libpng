bef02a89ea026537560df8db33c6c31a3e6b6c48
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-11-11 15:07:16 -0600

    [master] Bump to version 1.2.41beta19

--- pngtest.c
+++ pngtest.c
@@ -1689,4 +1689,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_41beta18 your_png_h_is_not_version_1_2_41beta18;
+typedef version_1_2_41beta19 your_png_h_is_not_version_1_2_41beta19;
