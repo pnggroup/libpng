11865f0e2940147e3a36e96fc1d4dc2700d61765
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-11-20 22:06:56 -0600

    [devel] Imported from libpng-1.4.0beta103.tar

--- pngwio.c
+++ pngwio.c
@@ -1,7 +1,7 @@
 
 /* pngwio.c - functions for data output
  *
- * Last changed in libpng 1.4.0 [November 20, 2009]
+ * Last changed in libpng 1.4.0 [November 21, 2009]
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
