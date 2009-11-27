e4b9fc28decc2641949f764f743ce41d1f3b0dac
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2009-11-27 00:23:07 -0600

    [devel] Imported from libpng-1.4.0beta106.tar

--- pngtest.c
+++ pngtest.c
@@ -1,7 +1,7 @@
 
 /* pngtest.c - a simple test program to test libpng
  *
- * Last changed in libpng 1.4.0 [November 26, 2009]
+ * Last changed in libpng 1.4.0 [November 27, 2009]
  * Copyright (c) 1998-2009 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
--- scripts/README.txt
+++ scripts/README.txt
@@ -1,5 +1,5 @@
 
-Makefiles for  libpng version 1.4.0beta106 - November 26, 2009
+Makefiles for  libpng version 1.4.0beta106 - November 27, 2009
 
  makefile.linux    =>  Linux/ELF makefile
                        (gcc, creates libpng14.so.14.1.4.0beta106)
