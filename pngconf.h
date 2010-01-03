2381cf3269386a18bc177c45c435aea5a907f8a7
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-01-03 16:57:14 -0600

    [master] Bump to version 1.4.1beta01

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.4.0 - January 3, 2010
+ * libpng version 1.4.1beta01 - January 3, 2010
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
