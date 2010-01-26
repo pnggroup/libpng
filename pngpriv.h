1655e4ddb0d3b9cc5663711c9b51f7cf931a00ec
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-01-26 06:33:37 -0600

    [master] Imported from libpng-1.4.1beta05.tar

--- pngpriv.h
+++ pngpriv.h
@@ -1,7 +1,7 @@
 
 /* pngpriv.h - private declarations for use inside libpng
  *
- * libpng version 1.4.1beta04 - January 23, 2010
+ * libpng version 1.4.1beta05 - January 26, 2010
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
