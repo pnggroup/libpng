7d6103ad6cc42c77561fe1905e83df9957091f6d
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-02-12 21:22:57 -0600

    [master] Imported from libpng-1.4.1beta11.tar

--- pngrtran.c
+++ pngrtran.c
@@ -1,7 +1,7 @@
 
 /* pngrtran.c - transforms the data in a row for PNG readers
  *
- * Last changed in libpng 1.4.1 [February 8, 2010]
+ * Last changed in libpng 1.4.1 [February 13, 2010]
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
