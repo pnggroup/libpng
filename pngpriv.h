f2c99bd5dc36069f77fd484fb638ccf4a3dc2369
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-03-09 08:40:52 -0600

    [devel] Removed unused png_measure_compressed_chunk() from pngpriv.h
    and libpngpf.3

--- pngpriv.h
+++ pngpriv.h
@@ -309,8 +309,6 @@ PNG_EXTERN void png_crc_read PNGARG((png_structp png_ptr, png_bytep buf,
 /* Decompress data in a chunk that uses compression */
 #if defined(PNG_zTXt_SUPPORTED) || defined(PNG_iTXt_SUPPORTED) || \
     defined(PNG_iCCP_SUPPORTED) || defined(PNG_sPLT_SUPPORTED)
-PNG_EXTERN png_size_t png_measure_decompressed_chunk PNGARG((png_structp
-    png_ptr, int comp_type, png_size_t chunklength, png_size_t prefix_length));
 PNG_EXTERN void png_decompress_chunk PNGARG((png_structp png_ptr,
     int comp_type, png_size_t chunklength, png_size_t prefix_length,
     png_size_t *data_length));
