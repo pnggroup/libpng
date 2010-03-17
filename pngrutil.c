b2eacd3e1f3513ab81736aeb86e760bd844b1680
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-03-17 07:31:48 -0500

    [devel] Imported from libpng-1.5.0beta15.tar

--- pngrutil.c
+++ pngrutil.c
@@ -1,7 +1,7 @@
 
 /* pngrutil.c - utilities to read a PNG file
  *
- * Last changed in libpng 1.4.1 [March 14, 2010]
+ * Last changed in libpng 1.4.1 [March 17, 2010]
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
