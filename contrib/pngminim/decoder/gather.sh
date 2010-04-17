6ff9ec81c552b75dda64daa77847464386265ef2
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-04-17 11:53:38 -0500

    [devel] Attempt to get contrib/pngminim working with new pnglibconf.h
    not quite working yet (pow is undefined)

--- contrib/pngminim/decoder/gather.sh
+++ contrib/pngminim/decoder/gather.sh
@@ -1,8 +1,23 @@
+
+# Get the source for the pngminus application
 cp ../../pngminus/png2pnm.c pngm2pnm.c
+
+# Get the libpng sources
 cp ../../../*.h .
 cp ../../../*.c .
+
+# Get the libpng scripts for building pnglibconf.h
+cp ../../../scripts/options.awk .
+cp ../../../scripts/pnglibconf.dfa .
+sed -e "s:scripts/::g" ../../../scripts/pnglibconf.mak > pnglibconf.mak
+#14+%
+# Remove libpng sources we won't use
 rm example.c pngtest.c pngpread.c pngw*.c
-# change the following 2 lines if zlib is somewhere else
+
+# Get the zlib sources
+# Change the following 2 lines if zlib is somewhere else
 cp ../../../../zlib/*.h .
 cp ../../../../zlib/*.c .
+
+# Remove zlib sources we won't use
 rm minigzip.c example.c compress.c deflate.c gz*
