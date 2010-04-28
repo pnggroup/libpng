e3e8f6bf02a124e2bd2c21721b58455d28db5a5c
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-04-28 07:54:14 -0500

    [devel] Revised contrib/pngminim to use the "everything off" option
    in pngusr.dfa.

--- contrib/pngminim/decoder/pngusr.h
+++ contrib/pngminim/decoder/pngusr.h
@@ -16,64 +16,17 @@
   /* No 16-bit support beyond reading with strip_16 */
 #endif
 
-#define PNG_USER_PRIVATEBUILD "PNG minimal build"
-#define PNG_USER_DLLFNAME_POSTFIX "MN"
+/* If pngusr.h is included during the build the following must
+ * be defined either here or in the .dfa file (pngusr.dfa in
+ * this case).  To include pngusr.h set -DPNG_USER_CONFIG in
+ * CPPFLAGS
+ */
+#define PNG_USER_PRIVATEBUILD "libpng minimal conformant PNG decoder"
+#define PNG_USER_DLLFNAME_POSTFIX "mr"
 
+/* List options to turn off features of the build that do not
+ * affect the API (so are not recorded in pnglibconf.h)
+ */
 #define PNG_NO_WARNINGS
-#define PNG_NO_ERROR_TEXT
-#define PNG_NO_READ_INT_FUNCTIONS
-
-#define PNG_NO_READ_BGR
-#define PNG_NO_READ_GAMMA
-#define PNG_NO_READ_BACKGROUND
-#define PNG_NO_READ_QUANTIZE
-#define PNG_NO_READ_INVERT
-#define PNG_NO_READ_SHIFT
-#define PNG_NO_READ_PACK
-#define PNG_NO_READ_PACKSWAP
-#define PNG_NO_READ_FILLER
-#define PNG_NO_READ_SWAP
-#define PNG_NO_READ_SWAP_ALPHA
-#define PNG_NO_READ_INVERT_ALPHA
-#define PNG_NO_READ_RGB_TO_GRAY
-#define PNG_NO_READ_USER_TRANSFORM
-#define PNG_NO_READ_bKGD
-#define PNG_NO_READ_cHRM
-#define PNG_NO_READ_gAMA
-#define PNG_NO_READ_hIST
-#define PNG_NO_READ_iCCP
-#define PNG_NO_READ_pCAL
-#define PNG_NO_READ_pHYs
-#define PNG_NO_READ_sBIT
-#define PNG_NO_READ_sCAL
-#define PNG_NO_READ_sPLT
-#define PNG_NO_READ_sRGB
-#define PNG_NO_READ_TEXT
-#define PNG_NO_READ_tIME
-#define PNG_NO_READ_UNKNOWN_CHUNKS
-#define PNG_NO_READ_USER_CHUNKS
-#define PNG_NO_READ_EMPTY_PLTE
-#define PNG_NO_READ_OPT_PLTE
-#define PNG_NO_READ_STRIP_ALPHA
-#define PNG_NO_READ_oFFs
-
-#define PNG_NO_WRITE_SUPPORTED
-
-#define PNG_NO_INFO_IMAGE
-#define PNG_NO_IO_STATE
-#define PNG_NO_USER_MEM
-#define PNG_NO_FIXED_POINT_SUPPORTED
-#define PNG_NO_MNG_FEATURES
-#define PNG_NO_USER_TRANSFORM_PTR
-#define PNG_NO_HANDLE_AS_UNKNOWN
-#define PNG_NO_CONSOLE_IO
-#define PNG_NO_ZALLOC_ZERO
-#define PNG_NO_ERROR_NUMBERS
-#define PNG_NO_EASY_ACCESS
-#define PNG_NO_PROGRESSIVE_READ
-#define PNG_NO_USER_LIMITS
-#define PNG_NO_SET_USER_LIMITS
-#define PNG_NO_TIME_RFC1123
 
 #endif /* MINRDPNGCONF_H */
-
