191fb4f5bacfd9f764588c8779778ed20f9136df
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-06-18 21:47:36 -0500

    [legacy] Imported from libpng-1.2.44beta02.tar

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.2.44beta01 - June 18, 2010
+ * libpng version 1.2.44beta02 - June 19, 2010
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
