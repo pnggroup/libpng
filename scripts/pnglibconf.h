5b3d554e1e8eb905bed7a97600d816c26f28867a
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-06-18 21:55:53 -0500

    [devel] Imported from libpng-1.5.0beta26.tar

--- scripts/pnglibconf.h
+++ scripts/pnglibconf.h
@@ -1,7 +1,7 @@
-/* libpng-1.5.0beta25 STANDARD API DEFINITION */
+/* libpng-1.5.0beta26 STANDARD API DEFINITION */
 /* pnglibconf.h - library build configuration */
 
-/* last changed in libpng version 1.5.0 - June 18, 2010 */
+/* last changed in libpng version 1.5.0 - June 19, 2010 */
 
 /* Copyright (c) 1998-2010 Glenn Randers-Pehrson */
 
