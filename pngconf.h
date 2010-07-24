5fd46fb8f7abd8de3fecf09a890c60af3fa2cbe5
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-07-24 08:09:12 -0500

    [master] Moved the definition of png_snprintf() outside of the enclosing
    "#ifdef" blocks in pngconf.h

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.5.0beta35 - July 12, 2010
+ * libpng version 1.5.0beta35 - July 24, 2010
  *
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
@@ -542,29 +542,30 @@ typedef char            FAR * FAR * FAR * png_charppp;
 #    define png_memcpy  memcpy
 #    define png_memset  memset
 #    define png_sprintf sprintf
-#    ifndef PNG_NO_SNPRINTF
-#      ifdef _MSC_VER
-#        define png_snprintf _snprintf   /* Added to v 1.2.19 */
-#        define png_snprintf2 _snprintf
-#        define png_snprintf6 _snprintf
-#      else
-#        define png_snprintf snprintf   /* Added to v 1.2.19 */
-#        define png_snprintf2 snprintf
-#        define png_snprintf6 snprintf
-#      endif
-#    else
-      /* You don't have or don't want to use snprintf().  Caution: Using
-       * sprintf instead of snprintf exposes your application to accidental
-       * or malevolent buffer overflows.  If you don't have snprintf()
-       * as a general rule you should provide one (you can get one from
-       * Portable OpenSSH).
-       */
-#      define png_snprintf(s1,n,fmt,x1) sprintf(s1,fmt,x1)
-#      define png_snprintf2(s1,n,fmt,x1,x2) sprintf(s1,fmt,x1,x2)
-#      define png_snprintf6(s1,n,fmt,x1,x2,x3,x4,x5,x6) \
-          sprintf(s1,fmt,x1,x2,x3,x4,x5,x6)
-#    endif
 #  endif
+#endif
+
+#ifndef PNG_NO_SNPRINTF
+#  ifdef _MSC_VER
+#    define png_snprintf _snprintf   /* Added to v 1.2.19 */
+#    define png_snprintf2 _snprintf
+#    define png_snprintf6 _snprintf
+#  else
+#    define png_snprintf snprintf   /* Added to v 1.2.19 */
+#    define png_snprintf2 snprintf
+#    define png_snprintf6 snprintf
+#  endif
+#else
+  /* You don't have or don't want to use snprintf().  Caution: Using
+   * sprintf instead of snprintf exposes your application to accidental
+   * or malevolent buffer overflows.  If you don't have snprintf()
+   * as a general rule you should provide one (you can get one from
+   * Portable OpenSSH).
+   */
+#  define png_snprintf(s1,n,fmt,x1) png_sprintf(s1,fmt,x1)
+#  define png_snprintf2(s1,n,fmt,x1,x2) png_sprintf(s1,fmt,x1,x2)
+#  define png_snprintf6(s1,n,fmt,x1,x2,x3,x4,x5,x6) \
+      png_sprintf(s1,fmt,x1,x2,x3,x4,x5,x6)
 #endif
 
 /* png_alloc_size_t is guaranteed to be no smaller than png_size_t,
