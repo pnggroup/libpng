6c47e84d2e9dd1d98108991b51356e48af2526a9
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-07-29 19:00:42 -0500

    [devel] Bump to version libpng-1.5.0beta36

--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.5.0beta36 - July 29, 2010
+libpng version 1.5.0beta36 - July 30, 2010
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
--- scripts/pnglibconf.h
+++ scripts/pnglibconf.h
@@ -1,7 +1,7 @@
 /* libpng-1.5.0beta36 STANDARD API DEFINITION */
 /* pnglibconf.h - library build configuration */
 
-/* last changed in libpng version 1.5.0 - July 29, 2010 */
+/* last changed in libpng version 1.5.0 - July 30, 2010 */
 
 /* Copyright (c) 1998-2010 Glenn Randers-Pehrson */
 
