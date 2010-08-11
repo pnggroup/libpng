2a718a3f538f633eed49aebe7cdd45b606eee518
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-08-11 09:02:49 -0500

    [devel] Bump to version libpng-1.5.0beta42

--- pngtest.c
+++ pngtest.c
@@ -1728,4 +1728,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_5_0beta41 your_png_h_is_not_version_1_5_0beta41;
+typedef version_1_5_0beta42 your_png_h_is_not_version_1_5_0beta42;
