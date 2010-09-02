87bc4b08ffe683125ad8b4188ecafe821a4add6f
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-09-02 06:39:52 -0500

    [master] Imported from libpng-1.4.4beta08.tar

--- png.h
+++ png.h
@@ -1,7 +1,7 @@
 
 /* png.h - header file for PNG reference library
  *
- * libpng version 1.4.4beta08 - August 26, 2010
+ * libpng version 1.4.4beta08 - September 2, 2010
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -11,7 +11,7 @@
  * Authors and maintainers:
  *  libpng versions 0.71, May 1995, through 0.88, January 1996: Guy Schalnat
  *  libpng versions 0.89c, June 1996, through 0.96, May 1997: Andreas Dilger
- *  libpng versions 0.97, January 1998, through 1.4.4beta08 - August 26, 2010: Glenn
+ *  libpng versions 0.97, January 1998, through 1.4.4beta08 - September 2, 2010: Glenn
  *  See also "Contributing Authors", below.
  *
  * Note about libpng version numbers:
@@ -177,7 +177,7 @@
  *
  * This code is released under the libpng license.
  *
- * libpng versions 1.2.6, August 15, 2004, through 1.4.4beta08, August 26, 2010, are
+ * libpng versions 1.2.6, August 15, 2004, through 1.4.4beta08, September 2, 2010, are
  * Copyright (c) 2004, 2006-2010 Glenn Randers-Pehrson, and are
  * distributed according to the same disclaimer and license as libpng-1.2.5
  * with the following individual added to the list of Contributing Authors:
@@ -289,7 +289,7 @@
  * Y2K compliance in libpng:
  * =========================
  *
- *    August 26, 2010
+ *    September 2, 2010
  *
  *    Since the PNG Development group is an ad-hoc body, we can't make
  *    an official declaration.
@@ -353,7 +353,7 @@
 /* Version information for png.h - this should match the version in png.c */
 #define PNG_LIBPNG_VER_STRING "1.4.4beta08"
 #define PNG_HEADER_VERSION_STRING \
-   " libpng version 1.4.4beta08 - August 26, 2010\n"
+   " libpng version 1.4.4beta08 - September 2, 2010\n"
 
 #define PNG_LIBPNG_VER_SONUM   14
 #define PNG_LIBPNG_VER_DLLNUM  14
