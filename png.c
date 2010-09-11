4f8e7e5f41b1de8a7b28cf50dc428feee79ef729
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-09-10 22:07:45 -0500

    [devel] Bump to version libpng-1.5.0beta48

--- png.c
+++ png.c
@@ -14,7 +14,7 @@
 #include "pngpriv.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_5_0beta47 Your_png_h_is_not_version_1_5_0beta47;
+typedef version_1_5_0beta48 Your_png_h_is_not_version_1_5_0beta48;
 
 /* Version information for C files.  This had better match the version
  * string defined in png.h.
@@ -560,13 +560,13 @@ png_get_copyright(png_structp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.5.0beta47 - September 11, 2010" PNG_STRING_NEWLINE \
+     "libpng version 1.5.0beta48 - September 11, 2010" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2010 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.5.0beta47 - September 11, 2010\
+      return "libpng version 1.5.0beta48 - September 11, 2010\
       Copyright (c) 1998-2010 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
