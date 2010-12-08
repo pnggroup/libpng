d5e3590df7a231113a5cbfc4846503ac24364a80
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-12-08 17:26:50 -0600

    [devel] Don't display test results (except PASS/FAIL) when running "make test".
    Instead put them in pngtest-log.txt

--- test-pngvalid-full.sh
+++ test-pngvalid-full.sh
@@ -2,6 +2,11 @@
 #
 # Run a sequence of gamma tests quietly
 err=0
+
+echo >> pngtest-log.txt
+echo "============ pngvalid-full.sh ==============" >> pngtest-log.txt
+
+echo "Running test-pngvalid-full.sh -- it's lengthy, please wait..."
 for gamma in threshold transform sbit 16-to-8
 do
    opts=
@@ -10,13 +15,15 @@ do
    test "$gamma" = sbit      || opts="$opts --nogamma-sbit"
    test "$gamma" = 16-to-8   || opts="$opts --nogamma-16-to-8"
 
-   if ./pngvalid -q --nostandard $opts
+   if ./pngvalid --nostandard $opts >> pngtest-log.txt
    then
-      echo "PASS:" pngvalid "(gamma-$gamma)"
+      echo "  PASS:" pngvalid "(gamma-$gamma)"
    else
-      echo "FAIL:" pngvalid "(gamma-$gamma)"
+      echo "  FAIL:" pngvalid "(gamma-$gamma)"
       err=1
    fi
 done
+
+echo
 
 exit $err
--- test-pngvalid-simple.sh
+++ test-pngvalid-simple.sh
@@ -4,14 +4,17 @@
 # gamma tests
 err=0
 
+echo >> pngtest-log.txt
+echo "============ pngvalid-simple.sh ==============" >> pngtest-log.txt
+echo "Running test-pngvalid-simple.sh"
 for opts in "" --progressive-read --interlace \
    "--progressive-read --interlace"
 do
-   if ./pngvalid -q --nogamma $opts
+   if ./pngvalid  --nogamma $opts >> pngtest-log.txt
    then
-      echo "PASS:" pngvalid --nogamma $opts
+      echo "  PASS:" pngvalid --nogamma $opts
    else
-      echo "FAIL:" pngvalid --nogamma $opts
+      echo "  FAIL:" pngvalid --nogamma $opts
       err=1
    fi
 done
