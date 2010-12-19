d3b76572c7524525efb73716974da666a01c49aa
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2010-12-19 16:59:47 -0600

    [devel] Bump to version libpng-1.5.0beta59

--- pngpriv.h
+++ pngpriv.h
@@ -1,7 +1,7 @@
 
 /* pngpriv.h - private declarations for use inside libpng
  *
- * libpng version 1.5.0beta58 - (PENDING RELEASE)
+ * libpng version 1.5.0beta59 - (PENDING RELEASE)
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2010 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
