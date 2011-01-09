3200e1e0889db7013b72138f022046f041b252c8
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-01-08 21:00:56 -0600

    [devel] Bump to version libpng-1.5.1beta02

--- png.h
+++ png.h
@@ -1,7 +1,7 @@
 
 /* png.h - header file for PNG reference library
  *
- * libpng version 1.5.1beta01 - January 8, 2011
+ * libpng version 1.5.1beta02 - January 9, 2011
  * Copyright (c) 1998-2011 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -11,7 +11,7 @@
  * Authors and maintainers:
  *   libpng versions 0.71, May 1995, through 0.88, January 1996: Guy Schalnat
  *   libpng versions 0.89c, June 1996, through 0.96, May 1997: Andreas Dilger
- *   libpng versions 0.97, January 1998, through 1.5.1beta01 - January 8, 2011: Glenn
+ *   libpng versions 0.97, January 1998, through 1.5.1beta02 - January 9, 2011: Glenn
  *   See also "Contributing Authors", below.
  *
  * Note about libpng version numbers:
@@ -144,7 +144,7 @@
  *    1.5.0beta01-58          15    10500  15.so.15.0[.0]
  *    1.5.0rc01-07            15    10500  15.so.15.0[.0]
  *    1.5.0                   15    10500  15.so.15.0[.0]
- *    1.5.1beta01             15    10501  15.so.15.1[.0]
+ *    1.5.1beta01-02          15    10501  15.so.15.1[.0]
  *
  *   Henceforth the source version will match the shared-library major
  *   and minor numbers; the shared-library major version number will be
@@ -176,7 +176,7 @@
  *
  * This code is released under the libpng license.
  *
- * libpng versions 1.2.6, August 15, 2004, through 1.5.1beta01, January 8, 2011, are
+ * libpng versions 1.2.6, August 15, 2004, through 1.5.1beta02, January 9, 2011, are
  * Copyright (c) 2004, 2006-2011 Glenn Randers-Pehrson, and are
  * distributed according to the same disclaimer and license as libpng-1.2.5
  * with the following individual added to the list of Contributing Authors:
@@ -288,13 +288,13 @@
  * Y2K compliance in libpng:
  * =========================
  *
- *    January 8, 2011
+ *    January 9, 2011
  *
  *    Since the PNG Development group is an ad-hoc body, we can't make
  *    an official declaration.
  *
  *    This is your unofficial assurance that libpng from version 0.71 and
- *    upward through 1.5.1beta01 are Y2K compliant.  It is my belief that
+ *    upward through 1.5.1beta02 are Y2K compliant.  It is my belief that
  *    earlier versions were also Y2K compliant.
  *
  *    Libpng only has three year fields.  One is a 2-byte unsigned integer
@@ -350,9 +350,9 @@
  */
 
 /* Version information for png.h - this should match the version in png.c */
-#define PNG_LIBPNG_VER_STRING "1.5.1beta01"
+#define PNG_LIBPNG_VER_STRING "1.5.1beta02"
 #define PNG_HEADER_VERSION_STRING \
-     " libpng version 1.5.1beta01 - January 8, 2011\n"
+     " libpng version 1.5.1beta02 - January 9, 2011\n"
 
 #define PNG_LIBPNG_VER_SONUM   15
 #define PNG_LIBPNG_VER_DLLNUM  15
@@ -365,7 +365,7 @@
  * PNG_LIBPNG_VER_STRING, omitting any leading zero:
  */
 
-#define PNG_LIBPNG_VER_BUILD  01
+#define PNG_LIBPNG_VER_BUILD  02
 
 /* Release Status */
 #define PNG_LIBPNG_BUILD_ALPHA    1
@@ -507,7 +507,7 @@ extern "C" {
 /* This triggers a compiler error in png.c, if png.c and png.h
  * do not agree upon the version number.
  */
-typedef char* png_libpng_version_1_5_1beta01;
+typedef char* png_libpng_version_1_5_1beta02;
 
 /* Three color definitions.  The order of the red, green, and blue, (and the
  * exact size) is not important, although the size of the fields need to
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.5.1beta01 - January 8, 2011
+libpng version 1.5.1beta02 - January 9, 2011
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
