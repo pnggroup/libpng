973c493e1b710f7c572347e6150ed097aa9562a4
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-02-04 18:51:00 -0600

    [devel] Add __clang__ special case in pngconf.h

--- pngconf.h
+++ pngconf.h
@@ -287,10 +287,10 @@
  * PNG_EXPORT function for every compiler.
  */
 #ifndef PNG_FUNCTION
-#  ifdef __GNUC__
+#  if defined(__GNUC__) && !defined(__clang__)
 #     define PNG_FUNCTION(type, name, args, attributes)\
          attributes type name args
-#  else /* !GNUC */
+#  else /* !GNUC || clang */
 #     ifdef _MSC_VER
 #        define PNG_FUNCTION(type, name, args, attributes)\
          attributes type name args
@@ -347,7 +347,7 @@
    * functions in png.h will generate compiler warnings.  Added at libpng
    * version 1.2.41.
    */
-#  ifdef __GNUC__
+#  if defined(__GNUC__) && !defined(__clang__)
 #    ifndef PNG_USE_RESULT
 #      define PNG_USE_RESULT __attribute__((__warn_unused_result__))
 #    endif
