cad6798a8c71b6976704cf8fb1b768ec78858a56
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-02-13 06:10:34 -0600

    [devel] Imported from libpng-1.5.2beta01.tar

--- pngpriv.h
+++ pngpriv.h
@@ -6,7 +6,7 @@
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
  *
- * Last changed in libpng 1.5.0 [January 6, 2011]
+ * Last changed in libpng 1.5.2 [(PENDING RELEASE)]
  *
  * This code is released under the libpng license.
  * For conditions of distribution and use, see the disclaimer
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.5.2beta01 - February 4, 2011
+libpng version 1.5.2beta01 - February 13, 2011
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
