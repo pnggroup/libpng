11130874d4a1f012f5570ced4c3fc4075af4bcf5
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-03-31 13:42:23 -0500

    [master] Imported from libpng-1.4.6rc01.tar

--- pngtest.c
+++ pngtest.c
@@ -1687,4 +1687,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_4_6beta07 your_png_h_is_not_version_1_4_6beta07;
+typedef version_1_4_6rc01 your_png_h_is_not_version_1_4_6rc01;
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.4.6beta07 - March 22, 2011
+libpng version 1.4.6rc01 - March 31, 2011
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
