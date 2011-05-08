f3d737dad1ddbe1b75a79079d017f36f3053f90f
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-05-07 21:41:15 -0500

    [devel] Imported from libpng-1.5.3beta06.tar

--- png.c
+++ png.c
@@ -619,13 +619,13 @@ png_get_copyright(png_const_structp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.5.3beta06 - May 7, 2011" PNG_STRING_NEWLINE \
+     "libpng version 1.5.3beta06 - May 8, 2011" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2011 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.5.3beta06 - May 7, 2011\
+      return "libpng version 1.5.3beta06 - May 8, 2011\
       Copyright (c) 1998-2011 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.5.3beta06 - May 7, 2011
+libpng version 1.5.3beta06 - May 8, 2011
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
