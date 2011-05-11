d83530f45d96d4b9cac90cd4bb28893dabfa70dc
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-05-11 14:37:59 -0500

    [devel] Bump to version libpng-1.5.3beta08

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.5.3beta07 - May 11, 2011
+ * libpng version 1.5.3beta08 - May 11, 2011
  *
  * Copyright (c) 1998-2011 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.5.3beta07 - May 11, 2011
+libpng version 1.5.3beta08 - May 11, 2011
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
