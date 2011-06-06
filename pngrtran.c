cc1d4d0dbc727f60b7ae5320776c05e668fd40e3
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-06-05 23:26:14 -0500

    [master] Check for integer overflow in png_set_rgb_to_gray().

--- pngrtran.c
+++ pngrtran.c
@@ -660,10 +660,14 @@ void PNGAPI
 png_set_rgb_to_gray(png_structp png_ptr, int error_action, double red,
    double green)
 {
-   int red_fixed = (int)((float)red*100000.0 + 0.5);
-   int green_fixed = (int)((float)green*100000.0 + 0.5);
+   int red_fixed, green_fixed;
    if (png_ptr == NULL)
       return;
+   if (red > 21474.83647 || red < -21474.83648 ||
+       green > 21474.83647 || green < -21474.83648)
+      png_error(png_ptr, "ignoring out of range rgb_to_gray coefficients");
+   red_fixed = (int)((float)red*100000.0 + 0.5);
+   green_fixed = (int)((float)green*100000.0 + 0.5);
    png_set_rgb_to_gray_fixed(png_ptr, error_action, red_fixed, green_fixed);
 }
 #endif
