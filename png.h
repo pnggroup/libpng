fd73d04515a4274e569a72baab374a6ded556a27
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-06-13 13:25:21 -0500

    [devel] Moved definitions of PNG_HAVE_IHDR, PNG_AFTER_IDAT, and PNG_HAVE_PLTE
    outside of an unknown-chunk block in png.h because they are also
    needed for other uses.

--- png.h
+++ png.h
@@ -1,7 +1,7 @@
 
 /* png.h - header file for PNG reference library
  *
- * libpng version 1.5.3beta11 - June 11, 2011
+ * libpng version 1.5.3beta12 - June 13, 2011
  * Copyright (c) 1998-2011 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -11,7 +11,7 @@
  * Authors and maintainers:
  *   libpng versions 0.71, May 1995, through 0.88, January 1996: Guy Schalnat
  *   libpng versions 0.89c, June 1996, through 0.96, May 1997: Andreas Dilger
- *   libpng versions 0.97, January 1998, through 1.5.3beta11 - June 11, 2011: Glenn
+ *   libpng versions 0.97, January 1998, through 1.5.3beta12 - June 13, 2011: Glenn
  *   See also "Contributing Authors", below.
  *
  * Note about libpng version numbers:
@@ -152,7 +152,7 @@
  *    1.5.2                   15    10502  15.so.15.2[.0]
  *    1.5.3beta01-10          15    10503  15.so.15.3[.0]
  *    1.5.3rc01-02            15    10503  15.so.15.3[.0]
- *    1.5.3beta011            15    10503  15.so.15.3[.0]
+ *    1.5.3beta11-12          15    10503  15.so.15.3[.0]
  *
  *   Henceforth the source version will match the shared-library major
  *   and minor numbers; the shared-library major version number will be
@@ -184,7 +184,7 @@
  *
  * This code is released under the libpng license.
  *
- * libpng versions 1.2.6, August 15, 2004, through 1.5.3beta11, June 11, 2011, are
+ * libpng versions 1.2.6, August 15, 2004, through 1.5.3beta12, June 13, 2011, are
  * Copyright (c) 2004, 2006-2011 Glenn Randers-Pehrson, and are
  * distributed according to the same disclaimer and license as libpng-1.2.5
  * with the following individual added to the list of Contributing Authors:
@@ -296,13 +296,13 @@
  * Y2K compliance in libpng:
  * =========================
  *
- *    June 11, 2011
+ *    June 13, 2011
  *
  *    Since the PNG Development group is an ad-hoc body, we can't make
  *    an official declaration.
  *
  *    This is your unofficial assurance that libpng from version 0.71 and
- *    upward through 1.5.3beta11 are Y2K compliant.  It is my belief that
+ *    upward through 1.5.3beta12 are Y2K compliant.  It is my belief that
  *    earlier versions were also Y2K compliant.
  *
  *    Libpng only has two year fields.  One is a 2-byte unsigned integer
@@ -357,9 +357,9 @@
  */
 
 /* Version information for png.h - this should match the version in png.c */
-#define PNG_LIBPNG_VER_STRING "1.5.3beta11"
+#define PNG_LIBPNG_VER_STRING "1.5.3beta12"
 #define PNG_HEADER_VERSION_STRING \
-     " libpng version 1.5.3beta11 - June 11, 2011\n"
+     " libpng version 1.5.3beta12 - June 13, 2011\n"
 
 #define PNG_LIBPNG_VER_SONUM   15
 #define PNG_LIBPNG_VER_DLLNUM  15
@@ -372,7 +372,7 @@
  * PNG_LIBPNG_VER_STRING, omitting any leading zero:
  */
 
-#define PNG_LIBPNG_VER_BUILD  11
+#define PNG_LIBPNG_VER_BUILD  12
 
 /* Release Status */
 #define PNG_LIBPNG_BUILD_ALPHA    1
@@ -519,7 +519,7 @@ extern "C" {
 /* This triggers a compiler error in png.c, if png.c and png.h
  * do not agree upon the version number.
  */
-typedef char* png_libpng_version_1_5_3beta11;
+typedef char* png_libpng_version_1_5_3beta12;
 
 /* Three color definitions.  The order of the red, green, and blue, (and the
  * exact size) is not important, although the size of the fields need to
@@ -668,11 +668,6 @@ typedef struct png_unknown_chunk_t
     png_byte location; /* mode of operation at read time */
 }
 
-/* Values for the unknown chunk location byte */
-
-#define PNG_HAVE_IHDR  0x01
-#define PNG_HAVE_PLTE  0x02
-#define PNG_AFTER_IDAT 0x08
 
 png_unknown_chunk;
 typedef png_unknown_chunk FAR * png_unknown_chunkp;
@@ -680,7 +675,11 @@ typedef PNG_CONST png_unknown_chunk FAR * png_const_unknown_chunkp;
 typedef png_unknown_chunk FAR * FAR * png_unknown_chunkpp;
 #endif
 
+/* Values for the unknown chunk location byte */
 
+#define PNG_HAVE_IHDR  0x01
+#define PNG_HAVE_PLTE  0x02
+#define PNG_AFTER_IDAT 0x08
 
 /* The complete definition of png_info has, as of libpng-1.5.0,
  * been moved into a separate header file that is not accessible to
