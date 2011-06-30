78bb9dafe138cc7b806d3e8fdafeca0d6eb0f83e
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-06-30 18:59:45 -0500

    [devel] Imported from libpng-1.5.4rc01.tar

--- png.c
+++ png.c
@@ -14,7 +14,7 @@
 #include "pngpriv.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_5_4beta08 Your_png_h_is_not_version_1_5_4beta08;
+typedef png_libpng_version_1_5_4rc01 Your_png_h_is_not_version_1_5_4rc01;
 
 /* Tells libpng that we have already handled the first "num_bytes" bytes
  * of the PNG file signature.  If the PNG data is embedded into another
@@ -617,13 +617,13 @@ png_get_copyright(png_const_structp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.5.4beta08 - June 23, 2011" PNG_STRING_NEWLINE \
+     "libpng version 1.5.4rc01 - June 30, 2011" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2011 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.5.4beta08 - June 23, 2011\
+      return "libpng version 1.5.4rc01 - June 30, 2011\
       Copyright (c) 1998-2011 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
