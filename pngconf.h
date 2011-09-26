1a16735ce7ae95834e27d5415a21fa51c7790abf
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-09-26 16:39:03 -0500

    [libpng15] Imported from libpng-1.5.6beta02.tar

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.5.6beta02 - September 22, 2011
+ * libpng version 1.5.6beta02 - September 26, 2011
  *
  * Copyright (c) 1998-2011 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.5.6beta02 - September 22, 2011
+libpng version 1.5.6beta02 - September 26, 2011
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
