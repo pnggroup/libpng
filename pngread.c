3dbfd300ff6e39984820f48f724611e81bb69729
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-10-13 17:24:36 -0500

    [libpng15] Removed two redundant tests for unitialized row.

--- pngread.c
+++ pngread.c
@@ -348,13 +348,7 @@ png_read_update_info(png_structp png_ptr, png_infop info_ptr)
    if (png_ptr == NULL)
       return;
 
-   if (!(png_ptr->flags & PNG_FLAG_ROW_INIT))
-      png_read_start_row(png_ptr);
-
-   else
-      png_warning(png_ptr,
-          "Ignoring extra png_read_update_info() call;"
-          " row buffer not reallocated");
+   png_read_start_row(png_ptr);
 
 #ifdef PNG_READ_TRANSFORMS_SUPPORTED
    png_read_transform_info(png_ptr, info_ptr);
@@ -374,15 +368,8 @@ png_start_read_image(png_structp png_ptr)
 {
    png_debug(1, "in png_start_read_image");
 
-   if (png_ptr == NULL)
-      return;
-
-   if (!(png_ptr->flags & PNG_FLAG_ROW_INIT))
-      png_read_start_row(png_ptr);
-   else
-      png_warning(png_ptr,
-          "Ignoring extra png_start_read_image() call;"
-          " row buffer not reallocated");
+   if (png_ptr != NULL)
+     png_read_start_row(png_ptr);
 }
 #endif /* PNG_SEQUENTIAL_READ_SUPPORTED */
 
