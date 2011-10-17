f5c5e53bf569daa50dd2293aa6b23df14acfa380
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-10-17 07:58:24 -0500

    [libpng15] Imported from libpng-1.5.6beta06.tar

--- png.c
+++ png.c
@@ -645,13 +645,13 @@ png_get_copyright(png_const_structp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.5.6beta06 - October 14, 2011" PNG_STRING_NEWLINE \
+     "libpng version 1.5.6beta06 - October 17, 2011" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2011 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.5.6beta06 - October 14, 2011\
+      return "libpng version 1.5.6beta06 - October 17, 2011\
       Copyright (c) 1998-2011 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
--- scripts/README.txt
+++ scripts/README.txt
@@ -1,5 +1,5 @@
 
-Makefiles for  libpng version 1.5.6beta06 - October 12, 2011
+Makefiles for  libpng version 1.5.6beta06 - October 17, 2011
 
 pnglibconf.h.prebuilt       =>  Stores configuration settings
  makefile.linux    =>  Linux/ELF makefile
