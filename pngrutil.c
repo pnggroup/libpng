8501129c17213f32fecbed5975ed77247f513bc7
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-11-21 11:21:29 -0600

    [libpng15] Removed some "#if 0" blocks of test code that remained in pngrutil.c

--- pngrutil.c
+++ pngrutil.c
@@ -3503,19 +3503,12 @@ png_read_filter_row_sub(png_row_infop row_info, png_bytep row,
    png_size_t istop = row_info->rowbytes;
    unsigned int bpp = (row_info->pixel_depth + 7) >> 3;
    png_bytep rp = row + bpp;
-#if 0
-   png_bytep lp = row;
-#endif
 
    PNG_UNUSED(prev_row)
 
    for (i = bpp; i < istop; i++)
    {
-#if 0
-      *rp = (png_byte)(((int)(*rp) + (int)(*lp++)) & 0xff);
-#else
       *rp = (png_byte)(((int)(*rp) + (int)(*(rp-bpp))) & 0xff);
-#endif
       rp++;
    }
 }
@@ -3543,9 +3536,6 @@ png_read_filter_row_avg(png_row_infop row_info, png_bytep row,
    png_size_t i;
    png_bytep rp = row;
    png_const_bytep pp = prev_row;
-#if 0
-   png_bytep lp = row;
-#endif
    unsigned int bpp = (row_info->pixel_depth + 7) >> 3;
    png_size_t istop = row_info->rowbytes - bpp;
 
@@ -3560,11 +3550,7 @@ png_read_filter_row_avg(png_row_infop row_info, png_bytep row,
    for (i = 0; i < istop; i++)
    {
       *rp = (png_byte)(((int)(*rp) +
-#if 0
-         (int)(*pp++ + *lp++) / 2 ) & 0xff);
-#else
          (int)(*pp++ + *(rp-bpp)) / 2 ) & 0xff);
-#endif
 
       rp++;
    }
