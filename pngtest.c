67ee8cee391f01bfa631fa8f9a4a80065cbe7a68
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2011-12-22 08:21:00 -0600

    [libpng16] bump version to 1.6.0beta04

--- pngtest.c
+++ pngtest.c
@@ -1,7 +1,7 @@
 
 /* pngtest.c - a simple test program to test libpng
  *
- * Last changed in libpng 1.5.6 [November 3, 2011]
+ * Last changed in libpng 1.6.0 [(PENDING RELEASE)]
  * Copyright (c) 1998-2011 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -1669,4 +1669,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_0beta03 Your_png_h_is_not_version_1_6_0beta03;
+typedef png_libpng_version_1_6_0beta04 Your_png_h_is_not_version_1_6_0beta04;
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.6.0beta03 - December 22, 2011
+libpng version 1.6.0beta04 - December 22, 2011
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
