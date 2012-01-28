5cdff4144e0879de6465d3e28314cac816dbc01d
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-01-28 01:07:34 -0600

    [libpng16] Divide pngstest into separate runs for basic and transparent images.

--- test-pngstest.sh
+++ test-pngstest.sh
@@ -9,12 +9,20 @@ echo "============ pngstest.sh ==============" >> pngtest-log.txt
 echo "Running test-pngstest.sh on contrib/pngsuite/*.png"
 for opts in "" "--background"
 do
-   if ./pngstest --log "$@" $opts ${srcdir}/contrib/pngsuite/*.png \
+   if ./pngstest --log "$@" $opts ${srcdir}/contrib/pngsuite/bas*.png \
       >>pngtest-log.txt 2>&1
    then
-      echo "  PASS: pngstest $opts"
+      echo "  PASS: pngstest $opts (basic images)"
    else
-      echo "  FAIL: pngstest $opts"
+      echo "  FAIL: pngstest $opts (basic images)"
+      err=1
+   fi
+   if ./pngstest --log "$@" $opts ${srcdir}/contrib/pngsuite/ft*.png \
+      >>pngtest-log.txt 2>&1
+   then
+      echo "  PASS: pngstest $opts (transparent images)"
+   else
+      echo "  FAIL: pngstest $opts (transparent images)"
       err=1
    fi
 done
