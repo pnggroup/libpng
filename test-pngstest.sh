ce3c7330badb03e780b53bea280e3223fe31b5e2
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-02-04 07:35:34 -0600

    [libpng16] Revised test-pngstest.sh to report PASS/FAIL for each image.

--- test-pngstest.sh
+++ test-pngstest.sh
@@ -6,25 +6,19 @@ err=0
 echo >> pngtest-log.txt
 echo "============ pngstest.sh ==============" >> pngtest-log.txt
 
-echo "Running test-pngstest.sh on contrib/pngsuite/*.png"
-for opts in ""
+echo "Running test-pngstest.sh"
+for image in ${srcdir}/contrib/pngsuite/*.png
 do
-   if ./pngstest --log "$@" $opts ${srcdir}/contrib/pngsuite/bas*.png \
-      >>pngtest-log.txt 2>&1
-   then
-      echo "  PASS: pngstest $opts (basic images)"
-   else
-      echo "  FAIL: pngstest $opts (basic images)"
-      err=1
-   fi
-   if ./pngstest --log "$@" $opts ${srcdir}/contrib/pngsuite/ft*.png \
-      >>pngtest-log.txt 2>&1
-   then
-      echo "  PASS: pngstest $opts (transparent images)"
-   else
-      echo "  FAIL: pngstest $opts (transparent images)"
-      err=1
-   fi
+   for opts in ""
+   do
+      if ./pngstest --log "$@" $opts $image >>pngtest-log.txt 2>&1
+      then
+         echo "  PASS: pngstest $opts $image"
+      else
+         echo "  FAIL: pngstest $opts $image"
+         err=1
+      fi
+   done
 done
    
 exit $err
