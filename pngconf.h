e0e4104db48cd38eda1414718be92984e93b9f0e
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-02-17 13:45:12 -0600

    [libpng12] Imported from libpng-1.2.47rc01.tar

--- pngconf.h
+++ pngconf.h
@@ -1,8 +1,8 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.2.47beta01 - July 10, 2011
- * Copyright (c) 1998-2011 Glenn Randers-Pehrson
+ * libpng version 1.2.47rc01 - February 17, 2012
+ * Copyright (c) 1998-2012 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
  *
