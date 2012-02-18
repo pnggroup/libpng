4ae1de1114cb83c6f1462e9f76c8efc23f3924c9
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-02-18 14:05:39 -0600

    [libpng12] Imported from libpng-1.2.47.tar

--- pngrutil.c
+++ pngrutil.c
@@ -1,7 +1,7 @@
 
 /* pngrutil.c - utilities to read a PNG file
  *
- * Last changed in libpng 1.2.47 [February 17, 2012]
+ * Last changed in libpng 1.2.47 [February 18, 2012]
  * Copyright (c) 1998-2012 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
--- scripts/makefile.sco
+++ scripts/makefile.sco
@@ -12,7 +12,7 @@
 # Library name:
 LIBNAME = libpng12
 PNGMAJ = 0
-PNGMIN = 1.2.47rc01
+PNGMIN = 1.2.47
 PNGVER = $(PNGMAJ).$(PNGMIN)
 
 # Shared library names:
