9f3a6671b235eeb7f4247c9fdbec984c02504e65
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-03-06 16:41:00 -0600

    [libpng14] Fixed PNG_LIBPNG_BUILD_BASE_TYPE definition in png.h

--- png.h
+++ png.h
@@ -1,7 +1,7 @@
 
 /* png.h - header file for PNG reference library
  *
- * libpng version 1.4.10beta02 - February 27, 2012
+ * libpng version 1.4.10rc02 - March 6, 2012
  * Copyright (c) 1998-2011 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -11,7 +11,7 @@
  * Authors and maintainers:
  *  libpng versions 0.71, May 1995, through 0.88, January 1996: Guy Schalnat
  *  libpng versions 0.89c, June 1996, through 0.96, May 1997: Andreas Dilger
- *  libpng versions 0.97, January 1998, through 1.4.10beta02 - February 27, 2012: Glenn
+ *  libpng versions 0.97, January 1998, through 1.4.10rc02 - March 6, 2012: Glenn
  *  See also "Contributing Authors", below.
  *
  * Note about libpng version numbers:
@@ -164,7 +164,8 @@
  *    1.4.9beta01             14    10409  14.so.14.9[.0]
  *    1.4.9rc01               14    10409  14.so.14.9[.0]
  *    1.4.9                   14    10409  14.so.14.9[.0]
- *    1.4.10beta01-02         14    10410  14.so.14.10[.0]
+ *    1.4.10beta01            14    10410  14.so.14.10[.0]
+ *    1.4.10rc01-02           14    10410  14.so.14.10[.0]
  *
  *    Henceforth the source version will match the shared-library major
  *    and minor numbers; the shared-library major version number will be
@@ -174,7 +175,7 @@
  *    to the source version x.y.z (leading zeros in y and z).  Beta versions
  *    were given the previous public release number plus a letter, until
  *    version 1.0.6j; from then on they were given the upcoming public
- *    release number plus "betaNN" or "rcN".
+ *    release number plus "betaNN" or "rcNN".
  *
  *    Binary incompatibility exists only when applications make direct access
  *    to the info_ptr or png_ptr members through png.h, and the compiled
@@ -196,7 +197,7 @@
  *
  * This code is released under the libpng license.
  *
- * libpng versions 1.2.6, August 15, 2004, through 1.4.10beta02, February 27, 2012, are
+ * libpng versions 1.2.6, August 15, 2004, through 1.4.10rc02, March 6, 2012, are
  * Copyright (c) 2004, 2006-2010 Glenn Randers-Pehrson, and are
  * distributed according to the same disclaimer and license as libpng-1.2.5
  * with the following individual added to the list of Contributing Authors:
@@ -308,13 +309,13 @@
  * Y2K compliance in libpng:
  * =========================
  *
- *    February 27, 2012
+ *    March 6, 2012
  *
  *    Since the PNG Development group is an ad-hoc body, we can't make
  *    an official declaration.
  *
  *    This is your unofficial assurance that libpng from version 0.71 and
- *    upward through 1.4.10beta02 are Y2K compliant.  It is my belief that earlier
+ *    upward through 1.4.10rc02 are Y2K compliant.  It is my belief that earlier
  *    versions were also Y2K compliant.
  *
  *    Libpng only has three year fields.  One is a 2-byte unsigned integer
@@ -370,9 +371,9 @@
  */
 
 /* Version information for png.h - this should match the version in png.c */
-#define PNG_LIBPNG_VER_STRING "1.4.10beta02"
+#define PNG_LIBPNG_VER_STRING "1.4.10rc02"
 #define PNG_HEADER_VERSION_STRING \
-   " libpng version 1.4.10beta02 - February 27, 2012\n"
+   " libpng version 1.4.10rc02 - March 6, 2012\n"
 
 #define PNG_LIBPNG_VER_SONUM   14
 #define PNG_LIBPNG_VER_DLLNUM  14
@@ -402,7 +403,7 @@
 #define PNG_LIBPNG_BUILD_SPECIAL 32 /* Cannot be OR'ed with
                                        PNG_LIBPNG_BUILD_PRIVATE */
 
-#define PNG_LIBPNG_BUILD_BASE_TYPE PNG_LIBPNG_BUILD_BETA
+#define PNG_LIBPNG_BUILD_BASE_TYPE PNG_LIBPNG_BUILD_RC
 
 /* Careful here.  At one time, Guy wanted to use 082, but that would be octal.
  * We must not include leading zeros.
@@ -1496,7 +1497,7 @@ struct png_struct_def
 /* This triggers a compiler error in png.c, if png.c and png.h
  * do not agree upon the version number.
  */
-typedef png_structp version_1_4_10beta02;
+typedef png_structp version_1_4_10rc02;
 
 typedef png_struct FAR * FAR * png_structpp;
 
