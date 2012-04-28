6fb8433ecc3966c31bf056e1ffdf36f2a782e3a7
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-04-28 05:54:20 -0500

    [libpng15] Imported from libpng-1.5.11beta01.tar

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.5.11beta01 - April 12, 2012
+ * libpng version 1.5.11beta01 - April 28, 2012
  *
  * Copyright (c) 1998-2012 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- scripts/README.txt
+++ scripts/README.txt
@@ -1,5 +1,5 @@
 
-Makefiles for  libpng version 1.5.11beta01 - April 12, 2012
+Makefiles for  libpng version 1.5.11beta01 - April 28, 2012
 
 pnglibconf.h.prebuilt       =>  Stores configuration settings
  makefile.linux    =>  Linux/ELF makefile
