c924d3dd25e4cfbd516724ed56189044e24f9ae8
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-06-06 13:04:47 -0500

    [libpng15] Improved the efficiency of the new png_check_palette_indexes()
    function.

--- pngtrans.c
+++ pngtrans.c
@@ -708,7 +708,7 @@ png_do_check_palette_indexes(png_structp png_ptr, png_row_infop row_info)
          {
             for (; rp > png_ptr->row_buf; rp--)
             {
-               if (*rp >= png_ptr->num_palette_max)
+               if (*rp > png_ptr->num_palette_max)
                   png_ptr->num_palette_max = (int) *rp;
             }
 
