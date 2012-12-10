e936b9c7b8195c1d436e6ab8c70320afa2e4109a
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2012-12-10 10:06:26 -0600

    [libpng14] Imported from libpng-1.4.13beta01.tar

--- pngpriv.h
+++ pngpriv.h
@@ -1,7 +1,7 @@
 
 /* pngpriv.h - private declarations for use inside libpng
  *
- * libpng version 1.4.13beta01 - July 14, 2012
+ * libpng version 1.4.13beta01 - December 10, 2012
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2011 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.4.13beta01 - July 14, 2012
+libpng version 1.4.13beta01 - December 10, 2012
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
