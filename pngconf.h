9477ecdfa6fb1ac6b37959d3a2a668f948452582
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-01-01 07:45:42 -0600

    [libpng16] Updated copyright year to 2013 in newly-changed files

--- pngconf.h
+++ pngconf.h
@@ -1,9 +1,9 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.6.0beta36 - December 23, 2012
+ * libpng version 1.6.0beta36 - January 1, 2013
  *
- * Copyright (c) 1998-2012 Glenn Randers-Pehrson
+ * Copyright (c) 1998-2013 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
  *
