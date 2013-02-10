8849c000ea471289a1129a845a35ffb802c2f2b1
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-02-10 16:14:54 -0600

    [libpng15] Enclosed the new png_get_palette_max
    in #ifdef PNG_GET_PALETTE_MAX_SUPPORTED block, and revised pnglibconf.h
    and pnglibconf.h.prebuilt accordingly.

--- pngget.c
+++ pngget.c
@@ -1128,6 +1128,7 @@ png_get_io_chunk_name (png_structp png_ptr)
 #endif /* ?PNG_IO_STATE_SUPPORTED */
 
 #ifdef PNG_CHECK_FOR_INVALID_INDEX_SUPPORTED
+#ifdef PNG_GET_PALETTE_MAX_SUPPORTED
 int PNGAPI
 png_get_palette_max(png_const_structp png_ptr, png_const_infop info_ptr)
 {
@@ -1136,6 +1137,7 @@ png_get_palette_max(png_const_structp png_ptr, png_const_infop info_ptr)
 
    return (-1);
 }
+#endif
 #endif
 
 #endif /* PNG_READ_SUPPORTED || PNG_WRITE_SUPPORTED */
