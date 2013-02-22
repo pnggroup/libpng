c9f99f487b6e9b508e10bdeca79a8694d26cb930
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-02-22 17:13:45 -0600

    [libpng17] Imported from libpng-1.7.0beta03.tar

--- png.c
+++ png.c
@@ -691,13 +691,13 @@ png_get_copyright(png_const_structrp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.7.0beta03 - February 18, 2013" PNG_STRING_NEWLINE \
+     "libpng version 1.7.0beta03 - February 22, 2013" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2013 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.7.0beta03 - February 18, 2013\
+      return "libpng version 1.7.0beta03 - February 22, 2013\
       Copyright (c) 1998-2013 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
