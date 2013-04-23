9bdba4e630abadc4d0d87228e7a77026adb7ce26
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-04-23 18:30:51 -0500

    [libpng16] Imported from libpng-1.6.2rc05.tar

--- pngtest.c
+++ pngtest.c
@@ -1968,4 +1968,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_2rc04 Your_png_h_is_not_version_1_6_2rc04;
+typedef png_libpng_version_1_6_2rc05 Your_png_h_is_not_version_1_6_2rc05;
