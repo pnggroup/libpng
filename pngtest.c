3f677d1c6567e7287aa6d359b0a7e7ee75a3cfab
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-04-25 09:12:51 -0500

    [libpng16] Bump version to 1.6.3beta01

--- pngtest.c
+++ pngtest.c
@@ -1,7 +1,7 @@
 
 /* pngtest.c - a simple test program to test libpng
  *
- * Last changed in libpng 1.6.2 [April 25, 2013]
+ * Last changed in libpng 1.6.2 [(PENDING RELEASE)]
  * Copyright (c) 1998-2013 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -1968,4 +1968,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_2 Your_png_h_is_not_version_1_6_2;
+typedef png_libpng_version_1_6_3beta01 Your_png_h_is_not_version_1_6_3beta01;
