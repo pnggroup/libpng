497e7319f4ecf127f786551026090ece2f752b7c
John Bowler <jbowler@acm.org>
2013-07-02 22:57:40 -0500

    [libpng16] Only compile ARM code when PNG_READ_SUPPORTED is defined.

--- arm/arm_init.c
+++ arm/arm_init.c
@@ -16,6 +16,7 @@
 
 #include "../pngpriv.h"
 
+#ifdef PNG_READ_SUPPORTED
 #if PNG_ARM_NEON_OPT > 0
 #ifdef PNG_ARM_NEON_CHECK_SUPPORTED /* Do run-time checks */
 #include <signal.h> /* for sig_atomic_t */
@@ -217,3 +218,4 @@ png_init_filter_functions_neon(png_structp pp, unsigned int bpp)
    }
 }
 #endif /* PNG_ARM_NEON_OPT > 0 */
+#endif /* PNG_READ_SUPPORTED */
