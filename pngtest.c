eae431e51447e4b0ccc95e2e4cc02787147dfc18
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-08-21 19:07:23 -0500

    [libpng17] Bump version to 1.7.0beta18

--- pngtest.c
+++ pngtest.c
@@ -1964,4 +1964,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_7_0beta17 Your_png_h_is_not_version_1_7_0beta17;
+typedef png_libpng_version_1_7_0beta18 Your_png_h_is_not_version_1_7_0beta18;
