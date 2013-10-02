553b8f85d3392dd17ab2085503809208311bb029
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-10-02 13:27:28 -0500

    [libpng17] Bump version to 1.7.0beta20

--- pngtest.c
+++ pngtest.c
@@ -1966,4 +1966,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_7_0beta19 Your_png_h_is_not_version_1_7_0beta19;
+typedef png_libpng_version_1_7_0beta20 Your_png_h_is_not_version_1_7_0beta20;
