6962a430f6f89a61b7332b9361c860c20d364980
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-01-01 09:40:54 -0600

    [libpng12] Bump version to 1.2.51beta04

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.2.51beta03 - January 1, 2014
+ * libpng version 1.2.51beta04 - January 1, 2014
  * Copyright (c) 1998-2013 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
