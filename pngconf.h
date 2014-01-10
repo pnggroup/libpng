1982968dad5966dbd6f2f863e27af9f30d094e5b
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-01-10 08:04:15 -0600

    [libpng17] Bump version to 1.7.0beta28

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.7.0beta27 - January 10, 2014
+ * libpng version 1.7.0beta28 - January 10, 2014
  *
  * Copyright (c) 1998-2013 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
