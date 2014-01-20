dfa6aea8acab49ab1571bb70a239d03d47a41f34
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-01-20 11:19:42 -0600

    [libpng15] Imported from libpng-1.5.18beta05.tar

--- png.c
+++ png.c
@@ -658,13 +658,13 @@ png_get_copyright(png_const_structp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.5.18beta05 - January 12, 2014" PNG_STRING_NEWLINE \
+     "libpng version 1.5.18beta05 - January 20, 2014" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2014 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.5.18beta05 - January 12, 2014\
+      return "libpng version 1.5.18beta05 - January 20, 2014\
       Copyright (c) 1998-2014 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
