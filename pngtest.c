ee6be8733241b2f68dcb614701d70fb085cbd0d5
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-02-06 10:30:17 -0600

    [libpng12] Bump version to 1.2.52beta01

--- pngtest.c
+++ pngtest.c
@@ -1702,4 +1702,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_51 your_png_h_is_not_version_1_2_51;
+typedef version_1_2_52beta01 your_png_h_is_not_version_1_2_52beta01;
