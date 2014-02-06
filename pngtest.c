849b2c9cbfcb144ac06459cdad06cd0b652fbd7a
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-02-05 21:52:51 -0600

    [libpng12] Imported from libpng-1.2.51.tar

--- pngtest.c
+++ pngtest.c
@@ -1,7 +1,7 @@
 
 /* pngtest.c - a simple test program to test libpng
  *
- * Last changed in libpng 1.2.51 [February 3, 2014]
+ * Last changed in libpng 1.2.51 [February 6, 2014]
  * Copyright (c) 1998-2014 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -1702,4 +1702,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_2_51rc04 your_png_h_is_not_version_1_2_51rc04;
+typedef version_1_2_51 your_png_h_is_not_version_1_2_51;
