7acfe50855a7be6c84f2beca6437da28ed0c3294
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-05-19 21:13:56 -0500

    [libpng16] Bump version to 1.6.11beta07

--- png.c
+++ png.c
@@ -14,7 +14,7 @@
 #include "pngpriv.h"
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_11beta06 Your_png_h_is_not_version_1_6_11beta06;
+typedef png_libpng_version_1_6_11beta07 Your_png_h_is_not_version_1_6_11beta07;
 
 /* Tells libpng that we have already handled the first "num_bytes" bytes
  * of the PNG file signature.  If the PNG data is embedded into another
@@ -773,13 +773,13 @@ png_get_copyright(png_const_structrp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.6.11beta06 - May 19, 2014" PNG_STRING_NEWLINE \
+     "libpng version 1.6.11beta07 - May 20, 2014" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2014 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.6.11beta06 - May 19, 2014\
+      return "libpng version 1.6.11beta07 - May 20, 2014\
       Copyright (c) 1998-2014 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.6.11beta06 - May 19, 2014
+libpng version 1.6.11beta07 - May 20, 2014
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
