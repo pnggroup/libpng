225af8f363e6658f69379048a97117e2225620a9
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-06-13 12:55:43 -0500

    [libpng16] Removed "PNG_UNUSED()" inadvertently left in contrib/gregbook
    /readpng2.c from the previous update.

--- contrib/gregbook/readpng2.c
+++ contrib/gregbook/readpng2.c
@@ -474,7 +474,6 @@ static void readpng2_warning_handler(png_structp png_ptr, png_const_charp msg)
 {
     fprintf(stderr, "readpng2 libpng warning: %s\n", msg);
     fflush(stderr);
-    PNG_UNUSED(png_ptr)
     (void)png_ptr; /* Unused */
 }
 
