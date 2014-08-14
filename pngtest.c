60b6a64975a08aed00b88f43a9e9e83b29f91595
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-08-13 19:59:28 -0500

    [libpng15] Bump version to 1.5.19rc02

--- pngtest.c
+++ pngtest.c
@@ -1990,4 +1990,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_5_19rc01 Your_png_h_is_not_version_1_5_19rc01;
+typedef png_libpng_version_1_5_19rc02 Your_png_h_is_not_version_1_5_19rc02;
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.5.19rc01 - August 14, 2014
+libpng version 1.5.19rc02 - August 14, 2014
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
