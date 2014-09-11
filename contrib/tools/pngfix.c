3a05d2df0f624d76d0dd7ddf945d72a128894490
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-09-11 07:39:24 -0500

    [libpng16] Add "#include <setjmp.h>" to contrib/tools/pngfix.c (John Bowler)

--- contrib/tools/pngfix.c
+++ contrib/tools/pngfix.c
@@ -18,7 +18,6 @@
 #include <limits.h>
 #include <errno.h>
 #include <assert.h>
-#include <setjmp.h>
 
 #define implies(x,y) assert(!(x) || (y))
 
@@ -49,6 +48,9 @@
 #if PNG_LIBPNG_VER < 10603 /* 1.6.3 */
 #  error "pngfix will not work with libpng prior to 1.6.3"
 #endif
+
+#ifdef PNG_SETJMP_SUPPORTED
+#include <setjmp.h>
 
 #if defined(PNG_READ_SUPPORTED) && defined(PNG_EASY_ACCESS_SUPPORTED)
 /* zlib.h defines the structure z_stream, an instance of which is included
@@ -4035,3 +4037,12 @@ main(void)
    return 77;
 }
 #endif /* PNG_READ_SUPPORTED && PNG_EASY_ACCESS_SUPPORTED */
+#else /* No setjmp support */
+int
+main(void)
+{
+   fprintf(stderr, "pngfix does not work without setjmp support\n");
+   return 77;
+}
+#endif /* PNG_SETJMP_SUPPORTED */
+
