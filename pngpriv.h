2eb712e519275739c82e70f92609576a7812afb3
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2014-11-03 09:28:50 -0600

    [libpng16] Bump version to 1.6.15beta04

--- pngpriv.h
+++ pngpriv.h
@@ -6,7 +6,7 @@
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
  *
- * Last changed in libpng 1.6.10 [March 6, 1014]]
+ * Last changed in libpng 1.6.15 [(PENDING RELEASE)]]
  *
  * This code is released under the libpng license.
  * For conditions of distribution and use, see the disclaimer
