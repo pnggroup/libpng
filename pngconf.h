3e04e189d81a2079b313fb5e70ba6b10ec106e9e
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-01-02 09:20:21 -0600

    [libpng17] Bump version to 1.7.0beta47

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.7.0beta46 - January 2, 2015
+ * libpng version 1.7.0beta47 - January 2, 2015
  *
  * Copyright (c) 1998-2015 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- pngtest.c
+++ pngtest.c
@@ -2008,4 +2008,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_7_0beta46 Your_png_h_is_not_version_1_7_0beta46;
+typedef png_libpng_version_1_7_0beta47 Your_png_h_is_not_version_1_7_0beta47;
