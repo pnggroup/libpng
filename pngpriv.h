36c05b6b1ae149be31c99df64c67724f1bc08064
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-02-21 12:59:33 -0600

    [libpng14] Rebuilt configure scripts with automake-1.15 and libtool-2.4.6

--- pngpriv.h
+++ pngpriv.h
@@ -1,7 +1,7 @@
 
 /* pngpriv.h - private declarations for use inside libpng
  *
- * libpng version 1.4.15 - February 12, 2015
+ * libpng version 1.4.16beta01 - February 21, 2015
  * For conditions of distribution and use, see copyright notice in png.h
  * Copyright (c) 1998-2014 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- projects/vstudio/readme.txt
+++ projects/vstudio/readme.txt
@@ -1,7 +1,7 @@
 
 VisualStudio instructions
 
-libpng version 1.4.15 - February 12, 2015
+libpng version 1.4.16beta01 - February 21, 2015
 
 Copyright (c) 1998-2010 Glenn Randers-Pehrson
 
