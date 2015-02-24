74aa31a7c5bd353660731c8668a8618544f5f633
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-02-24 12:36:13 -0600

    [libpng15] Relocated a misplaced "}" in png_reciprocal2().

--- png.c
+++ png.c
@@ -661,13 +661,13 @@ png_get_copyright(png_const_structp png_ptr)
 #else
 #  ifdef __STDC__
    return PNG_STRING_NEWLINE \
-     "libpng version 1.5.22beta04 - February 21, 2015" PNG_STRING_NEWLINE \
+     "libpng version 1.5.22beta04 - February 24, 2015" PNG_STRING_NEWLINE \
      "Copyright (c) 1998-2015 Glenn Randers-Pehrson" PNG_STRING_NEWLINE \
      "Copyright (c) 1996-1997 Andreas Dilger" PNG_STRING_NEWLINE \
      "Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc." \
      PNG_STRING_NEWLINE;
 #  else
-      return "libpng version 1.5.22beta04 - February 21, 2015\
+      return "libpng version 1.5.22beta04 - February 24, 2015\
       Copyright (c) 1998-2015 Glenn Randers-Pehrson\
       Copyright (c) 1996-1997 Andreas Dilger\
       Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.";
@@ -2138,18 +2138,18 @@ png_reciprocal2(png_fixed_point a, png_fixed_point b)
 
       if (r <= 2147483647. && r >= -2147483648.)
          return (png_fixed_point)r;
-#else
-      /* This may overflow because the range of png_fixed_point isn't
-       * symmetric, but this API is only used for the product of file and
-       * screen gamma so it doesn't matter that the smallest number it can
-       * produce is 1/21474, not 1/100000
-       */
-      png_fixed_point res = png_product2(a, b);
-
-      if (res != 0)
-         return png_reciprocal(res);
-#endif
    }
+#else
+   /* This may overflow because the range of png_fixed_point isn't
+    * symmetric, but this API is only used for the product of file and
+    * screen gamma so it doesn't matter that the smallest number it can
+    * produce is 1/21474, not 1/100000
+    */
+   png_fixed_point res = png_product2(a, b);
+
+   if (res != 0)
+      return png_reciprocal(res);
+#endif
 
    return 0; /* overflow */
 }
