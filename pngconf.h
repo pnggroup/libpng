4293254bef4128c8ff4c4f191af9b23570ee7a45
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-06-06 16:35:48 -0500

    [libpng16] Bump version to 1.6.18beta08

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.6.18beta07, (PENDING RELEASE)
+ * libpng version 1.6.18beta08, (PENDING RELEASE)
  *
  * Copyright (c) 1998-2015 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
