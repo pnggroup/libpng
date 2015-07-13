91c2de79b9e08d44b21c8dc7d6ccfc022def8312
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-07-13 08:22:18 -0500

    [libpng15] Bump version to libpng-1.5.23rc03.tar

--- pngtest.c
+++ pngtest.c
@@ -2025,4 +2025,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_5_23rc02 Your_png_h_is_not_version_1_5_23rc02;
+typedef png_libpng_version_1_5_23rc03 Your_png_h_is_not_version_1_5_23rc03;
