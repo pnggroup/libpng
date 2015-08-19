627c2c8797be0ad4880c2742ffbee65bbaea764e
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-08-19 12:24:51 -0500

    [libpng14] Bump version to 1.4.17beta02

--- pngtest.c
+++ pngtest.c
@@ -1724,4 +1724,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef version_1_4_17beta01 your_png_h_is_not_version_1_4_17beta01;
+typedef version_1_4_17beta02 your_png_h_is_not_version_1_4_17beta02;
