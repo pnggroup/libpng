0cc11b86baccc84213944187f543696fdbd63960
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-09-19 19:57:59 -0500

    [libpng16] Update ANNOUNCE and CHANGES with recent pngstest updates.

--- contrib/libtests/pngstest.c
+++ contrib/libtests/pngstest.c
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2013-2015 John Cunningham Bowler
  *
- * Last changed in libpng 1.6.18 [July 23, 2015]
+ * Last changed in libpng 1.6.19 [(PENDING RELEASE)]
  *
  * This code is released under the libpng license.
  * For conditions of distribution and use, see the disclaimer
