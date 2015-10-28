82c5abc170ca2574b7f72187e38e1afe56e2702c
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-10-28 09:19:59 -0500

    [libpng14] Imported from libpng-1.4.17rc01.tar

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.4.17beta04, October 23, 2015
+ * libpng version 1.4.17rc01, October 28, 2015
  *
  * Copyright (c) 1998-2015 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
