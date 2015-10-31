9db953ad5b4117cc4bf26010fb607980fa93c030
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-10-31 08:50:05 -0500

    [libpng15] Imported from libpng-1.5.24rc01.tar

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.5.24beta03, October 15, 2015
+ * libpng version 1.5.24rc01, October 31, 2015
  *
  * Copyright (c) 1998-2013 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
