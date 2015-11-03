186dbebe98111ff19e266c31bc67d3b1855abae6
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-11-03 06:28:31 -0600

    [libpng12] Bump version to 1.2.54rc03

--- pngconf.h
+++ pngconf.h
@@ -1,7 +1,7 @@
 
 /* pngconf.h - machine configurable file for libpng
  *
- * libpng version 1.2.54rc02, October 31, 2015
+ * libpng version 1.2.54rc03, November 3, 2015
  *
  * Copyright (c) 1998-2015 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
--- scripts/makefile.sco
+++ scripts/makefile.sco
@@ -12,7 +12,7 @@
 # Library name:
 LIBNAME = libpng12
 PNGMAJ = 0
-PNGMIN = 1.2.54rc02
+PNGMIN = 1.2.54rc03
 PNGVER = $(PNGMAJ).$(PNGMIN)
 
 # Shared library names:
