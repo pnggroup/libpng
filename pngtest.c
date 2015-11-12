415d26805a38023a23583ec3687927c481945ba0
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-11-12 07:00:12 -0600

    [libpng16] Imported from libpng-1.6.19.tar

--- pngtest.c
+++ pngtest.c
@@ -1,7 +1,7 @@
 
 /* pngtest.c - a simple test program to test libpng
  *
- * Last changed in libpng 1.6.19 [(PENDING RELEASE)]
+ * Last changed in libpng 1.6.19 [November 12, 2015]
  * Copyright (c) 1998-2015 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -2057,4 +2057,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_19rc04 Your_png_h_is_not_version_1_6_19rc04;
+typedef png_libpng_version_1_6_19 Your_png_h_is_not_version_1_6_19;
