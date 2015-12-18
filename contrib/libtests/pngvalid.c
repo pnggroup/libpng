42b438b7dcf31dc4be9a6f9db4987bc1acacd0a6
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2015-12-17 20:51:13 -0600

    [libpng16] Update CHANGES and ANNOUNCE

--- contrib/libtests/pngvalid.c
+++ contrib/libtests/pngvalid.c
@@ -1,7 +1,7 @@
 
 /* pngvalid.c - validate libpng by constructing then reading png files.
  *
- * Last changed in libpng 1.6.21 [(PENDING RELEASE)]
+ * Last changed in libpng 1.5.27 [(PENDING RELEASE)]
  * Copyright (c) 2014-2015 Glenn Randers-Pehrson
  * Written by John Cunningham Bowler
  *
