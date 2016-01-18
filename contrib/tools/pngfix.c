95d2726eccd455a67c03586eb3db75fd7fd0fb27
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2016-01-18 09:24:23 -0600

    [libpng16] Update change dates in pngvalid.c and pngfix.c

--- contrib/tools/pngfix.c
+++ contrib/tools/pngfix.c
@@ -2,7 +2,7 @@
  *
  * Copyright (c) 2014-2016 John Cunningham Bowler
  *
- * Last changed in libpng 1.6.21 [(PENDING RELEASE)]
+ * Last changed in libpng 1.6.21 [January 15, 2016]
  *
  * This code is released under the libpng license.
  * For conditions of distribution and use, see the disclaimer
