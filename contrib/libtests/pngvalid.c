1cef0c0eaeda6485a6ad8f503bda10ad3b43b5ac
John Bowler <jbowler@acm.org>
2016-01-28 16:49:48 -0800

    pngvalid: backport autoconf exit(77) fix
    Signed-off-by: John Bowler <jbowler@acm.org>

--- contrib/libtests/pngvalid.c
+++ contrib/libtests/pngvalid.c
@@ -62,10 +62,10 @@
 /* 1.6.1 added support for the configure test harness, which uses 77 to indicate
  * a skipped test, in earlier versions we need to succeed on a skipped test, so:
  */
-#if PNG_LIBPNG_VER < 10601
-#  define SKIP 0
-#else
+#if PNG_LIBPNG_VER >= 10601 && defined(HAVE_CONFIG_H)
 #  define SKIP 77
+#else
+#  define SKIP 0
 #endif
 
 /* pngvalid requires write support and one of the fixed or floating point APIs.
