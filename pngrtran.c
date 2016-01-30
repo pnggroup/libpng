05591abeb1186c46737b5db596c83ee05d7f4b6d
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2016-01-30 10:08:47 -0600

    [libpng16] Updated CHANGES, ANNOUNCE, change date in pngrtran.c

--- pngrtran.c
+++ pngrtran.c
@@ -1,8 +1,8 @@
 
 /* pngrtran.c - transforms the data in a row for PNG readers
  *
- * Last changed in libpng 1.6.19 [November 12, 2015]
- * Copyright (c) 1998-2002,2004,2006-2015 Glenn Randers-Pehrson
+ * Last changed in libpng 1.6.22 [(PENDING RELEASE)]
+ * Copyright (c) 1998-2002,2004,2006-2016 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
  *
