bd6fb5534f262b477e4908b71cc4f2b2678f8661
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2016-05-16 19:39:55 -0500

    [libpng17] Update Makefile.am, fix typo in timepng.c

--- contrib/libtests/timepng.c
+++ contrib/libtests/timepng.c
@@ -44,9 +44,9 @@
 #  define voidcast(type, value) (value)
 #endif /* __cplusplus */
 
-#if (defined (CLOCK_PROCESS_CPUTIME_ID) &&
-     defined (PNG_SEQUENTIAL_READ_SUPPORTED) && defined(PNG_STDIO_SUPPORTED)\
-     && defined(PNG_EASY_ACCESS_SUPPORTED) && defined(PNG_INFO_IMAGE_SUPPORTED)
+#if defined (CLOCK_PROCESS_CPUTIME_ID) && \
+    defined (PNG_SEQUENTIAL_READ_SUPPORTED) && defined(PNG_STDIO_SUPPORTED) \
+    && defined(PNG_EASY_ACCESS_SUPPORTED) && defined(PNG_INFO_IMAGE_SUPPORTED)
 
 typedef struct
 {
