6bdaf38d1283bc6436f6887c656030fde798aee3
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2016-08-10 22:26:12 -0500

    [libpng16] Revert change to png_malloc_array().  It's not needed
    now that iCCP profile_length honors PNG_USER_CHUNK_MALLOC_MAX.

--- pngmem.c
+++ pngmem.c
@@ -1,7 +1,7 @@
 
 /* pngmem.c - stub functions for memory allocation
  *
- * Last changed in libpng 1.6.25 [(PENDING RELEASE)]
+ * Last changed in libpng 1.6.24 [August 4, 2016]
  * Copyright (c) 1998-2002,2004,2006-2014,2016 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -123,10 +123,7 @@ png_malloc_array,(png_const_structrp png_ptr, int nelements,
     size_t element_size),PNG_ALLOCATED)
 {
    if (nelements <= 0 || element_size == 0)
-   {
-      png_warning(png_ptr, "internal error: array alloc");
-      return NULL;
-   }
+      png_error(png_ptr, "internal error: array alloc");
 
    return png_malloc_array_checked(png_ptr, nelements, element_size);
 }
