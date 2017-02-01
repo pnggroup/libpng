b1be78460a93caf6badee87fc6031e2084dbb112
Vadim Barkov <neverscaired@gmail.com>
2017-02-01 13:58:54 +0000

    Fixed mixed tabs and spaces in contrib/powerpc/linux_aux.c

--- contrib/powerpc-vsx/linux_aux.c
+++ contrib/powerpc-vsx/linux_aux.c
@@ -10,9 +10,7 @@
  * STATUS: COMPILED
  * BUG REPORTS: png-mng-implement@sourceforge.net
  *
- * png_have_vsx implemented for Linux by reading the widely available
- * pseudo-file /proc/cpuinfo. Result is cached so if function will be called
- * multiple times only one reading is perfomed.
+ * png_have_vsx implemented for Linux by using the auxiliary vector mechanism.
  *
  * This code is strict ANSI-C and is probably moderately portable; it does
  * however use <stdio.h> and it assumes that /proc/cpuinfo is never localized.
@@ -26,7 +24,8 @@ png_have_vsx(png_structp png_ptr)
 {
    const unsigned long auxv = getauxval( AT_HWCAP );
    if(auxv & (PPC_FEATURE_HAS_ALTIVEC|PPC_FEATURE_HAS_VSX ))
-	return 1;
+      return 1;
    else
-	return 0;
+      return 0;
 }
+
