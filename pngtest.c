894a9869dd60a1774374696a112de71bf538c1da
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2017-08-03 18:14:02 -0500

    [libpng16] Bump version to 1.6.32beta10

--- pngtest.c
+++ pngtest.c
@@ -2130,4 +2130,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_32beta09 Your_png_h_is_not_version_1_6_32beta09;
+typedef png_libpng_version_1_6_32beta10 Your_png_h_is_not_version_1_6_32beta10;
