e7c0ec1e92ab9c6bfddeb3418bb9c8bd979eac27
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2017-08-24 15:03:14 -0500

    [libpng15] Imported from libpng-1.5.29.tar

--- pngtrans.c
+++ pngtrans.c
@@ -1,7 +1,7 @@
 
 /* pngtrans.c - transforms the data in a row (used by both readers and writers)
  *
- * Last changed in libpng 1.5.29 [(PENDING RELEASE)]
+ * Last changed in libpng 1.5.29 [August 24, 2017]
  * Copyright (c) 1998-2002,2004,2006-2014,2016-2017 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
