a3d1057a735d923626f1f6bdc0f662a13d0cba6f
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2017-10-11 16:58:39 -0500

    [libpng16] Fix typo in fuzzer (png_info_ptr should be info_ptr)

--- contrib/oss-fuzz/libpng_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_read_fuzzer.cc
@@ -169,7 +169,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   int passes = png_set_interlace_handling(png_handler.png_ptr);
   png_start_read_image(png_handler.png_ptr);
 
-  png_read_update_info(png_handler.png_ptr, png_handler.png_info_ptr);
+  png_read_update_info(png_handler.png_ptr, png_handler.info_ptr);
 
   png_handler.row_ptr = png_malloc(
       png_handler.png_ptr, png_get_rowbytes(png_handler.png_ptr,
