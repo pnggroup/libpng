52b15dc0445d9cdd8b1e112b9226f34887e93053
Alberto <barbaro.alberto@gmail.com>
2022-07-05 08:04:26 +0100

    Initial version of the fix for https://github.com/glennrp/libpng/issues/350

--- contrib/tools/pngfix.c
+++ contrib/tools/pngfix.c
@@ -3961,6 +3961,13 @@ main(int argc, const char **argv)
       {
          size_t outlen = strlen(*argv);
 
+	 if ( outlen > FILENAME_MAX ) {
+            fprintf(stderr, "%s: output file name too long: %s%s%s\n",
+                prog, prefix, *argv, suffix ? suffix : "");
+            global.status_code |= WRITE_ERROR;
+            continue;
+	 }
+
          if (outfile == NULL) /* else this takes precedence */
          {
             /* Consider the prefix/suffix options */
