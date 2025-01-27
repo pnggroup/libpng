c4b20d0a3a7a53a0480a4c21c68b2c1794512629
John Bowler <jbowler@acm.org>
2025-01-27 10:51:01 -0800

    test: add configuration tests and fix bugs
    The two new configuation tests, fixed.dfa and float-fixed.dfa verify
    that the 'standard' configuration of libpng works without floating point
    arithmetic.
    
    Signed-off-by: John Bowler <jbowler@acm.org>

--- png.c
+++ png.c
@@ -2931,6 +2931,30 @@ png_gamma_significant(png_fixed_point gamma_val)
        gamma_val > PNG_FP_1 + PNG_GAMMA_THRESHOLD_FIXED;
 }
 
+#ifndef PNG_FLOATING_ARITHMETIC_SUPPORTED
+/* A local convenience routine. */
+static png_fixed_point
+png_product2(png_fixed_point a, png_fixed_point b)
+{
+   /* The required result is a * b; the following preserves accuracy. */
+#ifdef PNG_FLOATING_ARITHMETIC_SUPPORTED /* Should now be unused */
+   double r = a * 1E-5;
+   r *= b;
+   r = floor(r+.5);
+
+   if (r <= 2147483647. && r >= -2147483648.)
+      return (png_fixed_point)r;
+#else
+   png_fixed_point res;
+
+   if (png_muldiv(&res, a, b, 100000) != 0)
+      return res;
+#endif
+
+   return 0; /* overflow */
+}
+#endif /* FLOATING_ARITHMETIC */
+
 png_fixed_point
 png_reciprocal2(png_fixed_point a, png_fixed_point b)
 {
