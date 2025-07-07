ad1af43e794e0f56db45e513c1048522bb3181b7
Filip Wasil <f.wasil@samsung.com>
2025-07-07 11:08:35 +0200

    riscv: Support only RVV 1.0
    This is a cherry-pick of commit 7916eb7ba08e97ac97c71784e15f78e3ffcd838c
    from branch 'libpng16'.
    
    Reviewed-by: John Bowler <jbowler@acm.org>
    Signed-off-by: Cosmin Truta <ctruta@gmail.com>

--- riscv/riscv_init.c
+++ riscv/riscv_init.c
@@ -12,24 +12,9 @@
 
 #ifdef PNG_READ_SUPPORTED
 
-#if PNG_RISCV_RVV_OPT > 0
+#if PNG_RISCV_RVV_IMPLEMENTATION > 0
 
 #include <riscv_vector.h>
-
-#include <signal.h>
-
-#ifndef PNG_RISCV_RVV_FILE
-#  if defined(__linux__)
-#    define PNG_RISCV_RVV_FILE "contrib/riscv-rvv/linux.c"
-#  else
-#    error "No support for run-time RISC-V Vector checking; use compile-time options"
-#  endif
-#endif
-
-static int png_have_rvv();
-#ifdef PNG_RISCV_RVV_FILE
-#  include PNG_RISCV_RVV_FILE
-#endif
 
 #ifndef PNG_ALIGNED_MEMORY_SUPPORTED
 #  error "ALIGNED_MEMORY is required; set: -DPNG_ALIGNED_MEMORY_SUPPORTED"
@@ -38,15 +23,8 @@ static int png_have_rvv();
 void
 png_init_filter_functions_rvv(png_structp pp, unsigned int bpp)
 {
+#if __riscv_v == 1000000
    png_debug(1, "in png_init_filter_functions_rvv");
-
-   static volatile sig_atomic_t no_rvv = -1; /* not checked */
-
-   if (no_rvv < 0)
-      no_rvv = !png_have_rvv();
-
-   if (no_rvv)
-      return;
 
    pp->read_filter[PNG_FILTER_VALUE_UP-1] = png_read_filter_row_up_rvv;
 
@@ -62,7 +40,11 @@ png_init_filter_functions_rvv(png_structp pp, unsigned int bpp)
       pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth4_rvv;
       pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub4_rvv;
    }
+#else
+   PNG_UNUSED(pp);
+   PNG_UNUSED(bpp);
+#endif /* __riscv_v == 1000000 */
 }
 
-#endif /* PNG_RISCV_RVV_OPT > 0 */
+#endif /* PNG_RISCV_RVV_IMPLEMENTATION > 0 */
 #endif /* PNG_READ_SUPPORTED */
