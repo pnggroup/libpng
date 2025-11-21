pnggroup/libpng

commit 816b008d8fcb9f741bcacdb29e72955914936856

riscv: Leverage `__riscv_v` in pngpriv.h
Reviewed-by: John Bowler <jbowler@acm.org>
Signed-off-by: Cosmin Truta <ctruta@gmail.com>

pngpriv.h
@@ -303,6 +303,7 @@
 #endif
 
 #if PNG_RISCV_RVV_OPT > 0
+#if __riscv_v >= 1000000 && __riscv_v < 1900000
 #  define PNG_FILTER_OPTIMIZATIONS png_init_filter_functions_rvv
 #  ifndef PNG_RISCV_RVV_IMPLEMENTATION
       /* Use the intrinsics code by default. */
@@ -310,7 +311,8 @@
 #  endif
 #else
 #  define PNG_RISCV_RVV_IMPLEMENTATION 0
-#endif
+#endif /* __riscv_v >= 1000000 && __riscv_v < 1900000 */
+#endif /* PNG_RISCV_RVV_OPT > 0 */
 
 /* Is this a build of a DLL where compilation of the object modules requires
  * different preprocessor settings to those required for a simple library?  If
@@ -1546,7 +1548,7 @@ PNG_INTERNAL_FUNCTION(void,png_read_filter_row_paeth4_lsx,(png_row_infop
     row_info, png_bytep row, png_const_bytep prev_row),PNG_EMPTY);
 #endif
 
-#if PNG_RISCV_RVV_OPT > 0
+#if PNG_RISCV_RVV_IMPLEMENTATION == 1
 PNG_INTERNAL_FUNCTION(void,png_read_filter_row_up_rvv,(png_row_infop
     row_info, png_bytep row, png_const_bytep prev_row),PNG_EMPTY);
 PNG_INTERNAL_FUNCTION(void,png_read_filter_row_sub3_rvv,(png_row_infop
@@ -2175,7 +2177,7 @@ PNG_INTERNAL_FUNCTION(void, png_init_filter_functions_lsx,
     (png_structp png_ptr, unsigned int bpp), PNG_EMPTY);
 #endif
 
-#  if PNG_RISCV_RVV_OPT > 0
+#  if PNG_RISCV_RVV_IMPLEMENTATION == 1
 PNG_INTERNAL_FUNCTION(void, png_init_filter_functions_rvv,
    (png_structp png_ptr, unsigned int bpp), PNG_EMPTY);
 #endif

riscv/riscv_init.c
@@ -12,7 +12,7 @@
 
 #ifdef PNG_READ_SUPPORTED
 
-#if PNG_RISCV_RVV_IMPLEMENTATION > 0
+#if PNG_RISCV_RVV_IMPLEMENTATION == 1
 
 #include <riscv_vector.h>
 
@@ -23,7 +23,6 @@
 void
 png_init_filter_functions_rvv(png_structp pp, unsigned int bpp)
 {
-#if __riscv_v == 1000000
    png_debug(1, "in png_init_filter_functions_rvv");
 
    pp->read_filter[PNG_FILTER_VALUE_UP-1] = png_read_filter_row_up_rvv;
@@ -40,11 +39,7 @@ png_init_filter_functions_rvv(png_structp pp, unsigned int bpp)
       pp->read_filter[PNG_FILTER_VALUE_PAETH-1] = png_read_filter_row_paeth4_rvv;
       pp->read_filter[PNG_FILTER_VALUE_SUB-1] = png_read_filter_row_sub4_rvv;
    }
-#else
-   PNG_UNUSED(pp);
-   PNG_UNUSED(bpp);
-#endif /* __riscv_v == 1000000 */
 }
 
-#endif /* PNG_RISCV_RVV_IMPLEMENTATION > 0 */
+#endif /* PNG_RISCV_RVV_IMPLEMENTATION == 1 */
 #endif /* PNG_READ_SUPPORTED */

commit 3a3d485a7564de4f1f348628864a774a4af16af8

riscv: Fix `-Wundef` compiler warnings and relax RVV version checks
Fix a regression from commit 816b008d8fcb9f741bcacdb29e72955914936856.

When the build was not optimized for RVV (`PNG_RISCV_RVV_OPT == 0`),
the macro PNG_RISCV_RVV_IMPLEMENTATION was left undefined, even though
it is checked with `#if` instead of `#ifdef` in the source code.
Additionally, the RVV version checks included an upper bound check
(`__riscv_v < 1900000`) that disabled the RVV-optimized path for any
future RVV 1.9+ version.

We added the missing fallback definition and we removed the v1.9+ upper
bound. The RVV optimizations are now enabled for any RISC-V compiler
that supports RVV 1.0 or later, following the expectation that minor
version updates shall maintain compatibility.

pngpriv.h
@@ -302,8 +302,7 @@
 #   define PNG_LOONGARCH_LSX_IMPLEMENTATION 0
 #endif
 
-#if PNG_RISCV_RVV_OPT > 0
-#if __riscv_v >= 1000000 && __riscv_v < 1900000
+#if PNG_RISCV_RVV_OPT > 0 && __riscv_v >= 1000000
 #  define PNG_FILTER_OPTIMIZATIONS png_init_filter_functions_rvv
 #  ifndef PNG_RISCV_RVV_IMPLEMENTATION
       /* Use the intrinsics code by default. */
@@ -311,8 +310,7 @@
 #  endif
 #else
 #  define PNG_RISCV_RVV_IMPLEMENTATION 0
-#endif /* __riscv_v >= 1000000 && __riscv_v < 1900000 */
-#endif /* PNG_RISCV_RVV_OPT > 0 */
+#endif /* PNG_RISCV_RVV_OPT > 0 && __riscv_v >= 1000000 */
 
 /* Is this a build of a DLL where compilation of the object modules requires
  * different preprocessor settings to those required for a simple library?  If