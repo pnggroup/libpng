5b159aed75e9d02d065026b8eeefcfc8b7c6b8e4
Cosmin Truta <ctruta@gmail.com>
2025-12-03 20:31:19 +0200

    riscv: Reformat the contributor list and update the AUTHORS file

--- riscv/filter_rvv_intrinsics.c
+++ riscv/filter_rvv_intrinsics.c
@@ -3,8 +3,8 @@
  * Copyright (c) 2023 Google LLC
  * Written by Manfred SCHLAEGL, 2022
  *            Dragoș Tiselice <dtiselice@google.com>, May 2023.
- *            Filip Wasil     <f.wasil@samsung.com>, March 2025.
- *            Liang Junzhao   <junzhao.liang@spacemit.com>, Nov 2025.
+ *            Filip Wasil <f.wasil@samsung.com>, March 2025.
+ *            Liang Junzhao <junzhao.liang@spacemit.com>, November 2025.
  *
  * This code is released under the libpng license.
  * For conditions of distribution and use, see the disclaimer
