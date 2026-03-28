2190aaab4676e99cbe16c6008bacb6e52c82051c
Jeffin820 <jeffinphilip14@gmail.com>
2026-03-28 14:24:21 +0530

    Update macros to C23 Inlines

--- pngstruct.h
+++ pngstruct.h
@@ -91,22 +91,28 @@ typedef enum
 
 /* Chunk flag values.  These are (png_uint_32 values) with exactly one bit set
  * and can be combined into a flag set with bitwise 'or'.
- *
- * TODO: C23: convert these macros to C23 inlines (which are static).
  */
-#define png_chunk_flag_from_index(i) (0x80000000U >> (31 - (i)))
-   /* The flag corresponding to the given png_index enum value.  This is defined
-    * for png_unknown as well (until it reaches the value 32) but this should
-    * not be relied on.
-    */
+static inline png_uint_32 png_chunk_flag_from_index(png_index i)
+{
+   return 0x80000000U >> (31 - (i));
+}
+/* The flag corresponding to the given png_index enum value.  This is defined
+ * for png_unknown as well (until it reaches the value 32) but this should
+ * not be relied on.
+ */
 
-#define png_file_has_chunk(png_ptr, i)\
-   (((png_ptr)->chunks & png_chunk_flag_from_index(i)) != 0)
-   /* The chunk has been recorded in png_struct */
+static inline png_uint_32 png_file_has_chunk(const png_struct *png_ptr,
+    png_index i)
+{
+   return ((png_ptr)->chunks & png_chunk_flag_from_index(i)) != 0;
+}
+/* The chunk has been recorded in png_struct */
 
-#define png_file_add_chunk(png_ptr, i)\
-   ((void)((png_ptr)->chunks |= png_chunk_flag_from_index(i)))
-   /* Record the chunk in the png_struct */
+static inline void png_file_add_chunk(png_struct *png_ptr, png_index i)
+{
+   (png_ptr)->chunks |= png_chunk_flag_from_index(i);
+}
+/* Record the chunk in the png_struct */
 
 struct png_struct_def
 {
