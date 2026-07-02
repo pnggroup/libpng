f019d6c31d588ccbeee6b539ba6883f34c844fbc
bibi samina <sam@bugqore.com>
2026-07-02 20:39:52 +0530

    fix display row pointer declaration in readpng and timepng

--- contrib/libtests/timepng.c
+++ contrib/libtests/timepng.c
@@ -84,7 +84,7 @@ static void read_by_row(png_struct *png_ptr, png_info *info_ptr,
    /* These don't get freed on error, this is fine; the program immediately
     * exits.
     */
-   png_byte *row = NULL, display = NULL;
+   png_byte *row = NULL, *display = NULL;
    io_data io_copy;
 
    if (write_ptr != NULL)
